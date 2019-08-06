2d6b48c5a83528a4818fd902ad7d2d38e5e1e194
Louise Davies <louise.davies@stfc.ac.uk>
2019-08-06 16:01:33 +0100

    #12 - add e2e tests for date filter

--- cypress/integration/datafiles.spec.ts
+++ cypress/integration/datafiles.spec.ts
@@ -72,7 +72,32 @@ describe('Datafiles Table', () => {
       );
     });
 
-    it('date between');
+    it('date between', () => {
+      cy.get('[aria-label="Modified Time date filter from"]').type(
+        '2019-01-01'
+      );
+
+      cy.get('[aria-label="Modified Time date filter to"]')
+        .parent()
+        .find('button')
+        .click();
+
+      cy.get('.MuiPickersDay-day[tabindex="0"]')
+        .first()
+        .click();
+
+      cy.contains('OK').click();
+
+      let date = new Date();
+      date.setDate(1);
+
+      cy.get('[aria-label="Modified Time date filter to"]').should(
+        'have.value',
+        date.toISOString().slice(0, 10)
+      );
+
+      cy.get('[aria-rowcount="100"]').should('exist');
+    });
 
     it('multiple columns', () => {
       cy.get('[aria-label="Filter by Name"]')
