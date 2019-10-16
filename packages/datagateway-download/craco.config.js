b4e603a4a453dbc769e4c53df368eb9777a7d4a4
Louise Davies <louise.davies@stfc.ac.uk>
2019-10-16 14:59:11 +0100

    #46 - initial plugin created + craco config

---
+++ packages/datagateway-download/craco.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.externals = {
+        react: 'React', // Case matters here
+        'react-dom': 'ReactDOM', // Case matters here
+      };
+
+      if (env === 'production' && !process.env.REACT_APP_E2E_TESTING) {
+        webpackConfig.output.library = 'datagateway_download';
+        webpackConfig.output.libraryTarget = 'window';
+
+        webpackConfig.output.filename = '[name].js';
+        webpackConfig.output.chunkFilename = '[name].chunk.js';
+
+        delete webpackConfig.optimization.splitChunks;
+        webpackConfig.optimization.runtimeChunk = false;
+      }
+
+      return webpackConfig;
+    },
+  },
+};
