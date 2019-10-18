d12d420d3a453a5869a110dda3a7b29f8d37b0c7
Goel Biju <goelmolel@hotmail.com>
2019-10-18 09:49:32 +0100

    Add single-spa-react hooks (#46)

--- packages/datagateway-download/src/index.tsx
+++ packages/datagateway-download/src/index.tsx
@@ -4,7 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import singleSpaReact from 'single-spa-react';
+
+function domElementGetter(): HTMLElement {
+    // Make sure there is a div for us to render into
+    let el = document.getElementById('datagateway-download');
+    if (!el) {
+        el = document.createElement('div');
+    }
+
+    return el;
+}
+
+const reactLifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
+    domElementGetter,
+});
+
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Single-SPA bootstrap methods have no idea what type of inputs may be
+// pushed down from the parent app
+export function bootstrap(props: any): Promise<void> {
+    return reactLifecycles.bootstrap(props);
+}
+  
+export function mount(props: any): Promise<void> {
+    return reactLifecycles.mount(props);
+}
+  
+export function unmount(props: any): Promise<void> {
+    return reactLifecycles.unmount(props);
+}
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+
+if (process.env.NODE_ENV === `development`) {
+    ReactDOM.render(<App />, document.getElementById('datagateway-download'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
