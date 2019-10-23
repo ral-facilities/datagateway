f9fda4e29fe60d25d5108fb2f68249eb18bc7f3f
Goel Biju <goelmolel@hotmail.com>
2019-10-23 15:06:19 +0100

    apiUrl and Location passed as props using RouteComponentProps (#51)

--- packages/datagateway-table/src/App.tsx
+++ packages/datagateway-table/src/App.tsx
@@ -104,8 +104,8 @@ class App extends React.Component<{}, { hasError: boolean }> {
           <ConnectedRouter history={history}>
             <StylesProvider generateClassName={generateClassName}>
               {/* TODO: Handle breadcrumbs for the page. */}
-              <PageBreadcrumbs location={window.location} />
-              <Switch>         
+              <PageBreadcrumbs />
+              <Switch>
                 <Route
                   exact
                   path="/"
--- packages/datagateway-table/src/breadcrumbs.component.tsx
+++ packages/datagateway-table/src/breadcrumbs.component.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { StateType, DGTableState } from './state/app.types';
-import { connect, useSelector } from 'react-redux';
+import { StateType } from './state/app.types';
+import { connect } from 'react-redux';
 
 import axios from 'axios';
-import { Route } from 'react-router';
-import {
-  Paper,
-  Breadcrumbs,
-  Link,
-  Typography,
-} from '@material-ui/core';
+import { Route, RouteComponentProps } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import { Paper, Breadcrumbs, Link, Typography } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 // TODO: Maintain internal component state.
@@ -29,166 +25,154 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 //   }
 // }
 
-const apiRoutes: { [entity: string]: string } = {
-  "proposal": "/investigations/",
-  "investigation": "/investigations/",
-  "dataset": "/datasets/",
-  "datafiles": "/datafiles/",
+const apiEntityRoutes: { [entity: string]: string } = {
+  proposal: '/investigations/',
+  investigation: '/investigations/',
+  dataset: '/datasets/',
+  datafiles: '/datafiles/',
 };
 
-
-interface BreadcrumbProps { 
+interface BreadcrumbProps extends RouteComponentProps {
   apiUrl: string;
-  location: Location;
-};
+}
 
 class PageBreadcrumbs extends React.Component<BreadcrumbProps> {
-
   public constructor(props: BreadcrumbProps) {
     super(props);
-
-    // Get the API url in use.
-    console.log("API Url: ", this.props.apiUrl);
   }
-  
 
   // Store the current breadcrumb state; use the IDs
   // of the investigation/dataset
   // const currentBreadcrumb = {
   // }
-  
-    // return (
-    //   <div>
-    //     <Paper elevation={0}>
-    //       <Breadcrumbs
-    //         separator={<NavigateNextIcon fontSize="small" />}
-    //         aria-label="breadcrumb"
-    //       >
-    //         <Link color="inherit">
-    //           Home
-    //         </Link>
-    //         {/* <MaterialLink color="inherit">
-    //                         Test2
-    //                     </MaterialLink> */}
-    //         <Typography color="textPrimary">breadcrumb</Typography>
-    //       </Breadcrumbs>
-    //     </Paper>
-    //   </div>
-    // );
 
-    public componentDidMount() {
-      console.log("Location: ", this.props.location);
-    }
+  // return (
+  //   <div>
+  //     <Paper elevation={0}>
+  //       <Breadcrumbs
+  //         separator={<NavigateNextIcon fontSize="small" />}
+  //         aria-label="breadcrumb"
+  //       >
+  //         <Link color="inherit">
+  //           Home
+  //         </Link>
+  //         {/* <MaterialLink color="inherit">
+  //                         Test2
+  //                     </MaterialLink> */}
+  //         <Typography color="textPrimary">breadcrumb</Typography>
+  //       </Breadcrumbs>
+  //     </Paper>
+  //   </div>
+  // );
 
-    private getEntityName = async (requestEntityUrl: string): Promise<string> => {
-      let entityName = '';
-      
-      // Make a GET request to the specified URL.
-      entityName = await axios.get(`${this.props.apiUrl}${requestEntityUrl}`, {
-          headers: {
-            Authorization: `Bearer ${window.localStorage.getItem('daaas:token')}`,
-          },
-        })
-          .then(response => {
-            console.log(`${requestEntityUrl} - Response Data:`, response.data);
+  public componentDidMount(): void {
+    // const { location } = this.props;
+    // console.log("Location Pathname: ", this.props.location.pathname);
+  }
 
-            // Return the NAME property in the data received.
-            console.log(`${requestEntityUrl} - Entity Name: ${response.data.NAME}`);
-            return response.data.NAME;
-          })
-          .catch(error => {
-            console.log(error);
-            return '';
-          });
+  private getEntityName = async (requestEntityUrl: string): Promise<string> => {
+    let entityName = '';
 
-      return entityName;
-    }
+    // Make a GET request to the specified URL.
+    entityName = await axios
+      .get(`${this.props.apiUrl}${requestEntityUrl}`, {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('daaas:token')}`,
+        },
+      })
+      .then(response => {
+        console.log(`${requestEntityUrl} - Response Data:`, response.data);
 
+        // Return the NAME property in the data received.
+        console.log(`${requestEntityUrl} - Entity Name: ${response.data.NAME}`);
+        return response.data.NAME;
+      })
+      .catch(error => {
+        console.log(error);
+        return '';
+      });
 
-    public render(): React.ReactElement {
-      return (
-        <div>
-          <Paper elevation={0}>
-            <Route>
-              {({ location }) => {
-                const pathnames = location.pathname.split('/').filter(x => x);
-                console.log(`Path names: ${pathnames}`);
+    return entityName;
+  };
 
-                return (
-                  <Breadcrumbs 
-                    separator={<NavigateNextIcon fontSize="small" />}
-                    aria-label="Breadcrumb"
-                  >
-                    <Link color="inherit" href="/">
-                      Browse
-                    </Link>
+  public render(): React.ReactElement {
+    return (
+      <div>
+        <Paper elevation={0}>
+          <Route>
+            {({ location }) => {
+              const pathnames = location.pathname.split('/').filter(x => x);
+              console.log(`Path names: ${pathnames}`);
 
-                    {/* For each of the names in the path, request the entity names from the API. */}
-                    {pathnames.map((value: string, index: number) => {
-                      console.log(`Current value: ${value}`);
-                      console.log(`Current index: ${index}`);
+              return (
+                <Breadcrumbs
+                  separator={<NavigateNextIcon fontSize="small" />}
+                  aria-label="Breadcrumb"
+                >
+                  <Link color="inherit" href="/">
+                    Browse
+                  </Link>
 
-                      // const last = index === pathnames.length - 1;
-                      const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-                      
-                      // Check for the specific routes and request the names from the API.
-                      // if not last and value is in apiRoutes
-                      if (value in apiRoutes) {
-                        let requestEntityUrl = `${apiRoutes[value]}${index}`;
-                        
-                        this.getEntityName(requestEntityUrl)
-                          .then(entityName => {
-                            if (entityName) {
-                              console.log(`${value} - Retrieved entity name: ${entityName}`);
+                  {/* For each of the names in the path, request the entity names from the API. */}
+                  {pathnames.map((value: string, index: number) => {
+                    console.log(`Current value: ${value}`);
+                    console.log(`Current index: ${index}`);
 
-                              // Return the Link with the entity name.
-                              return (
-                                // Include key?
-                                <Link color="inherit" href={to}>
-                                  {entityName}
-                                </Link>
-                              );
-                            }
-                          });
-                      }
-                      
-                      return (
-                        <Link>
-                          N/A
-                        </Link>
-                      )
-                      
-                      // return last ? (
-                      //  <Typography color="textPrimary" key={to}>
-                      //    {value}
-                      //  </Typography>
-                      // ) : (
+                    // const last = index === pathnames.length - 1;
+                    const to = `/${pathnames.slice(0, index + 1).join('/')}`;
 
-                      // return (
-                        // <Link color="inherit" href={to} key={to}>
-                        //   {value}
-                        // </Link>
-                      // );
-                      // );
-                    })}
-                  </Breadcrumbs>
-                );
-              }}
-            </Route>
-          </Paper>
-        </div>
-      );
+                    // Check for the specific routes and request the names from the API.
+                    // if not last and value is in apiRoutes
+                    if (value in apiEntityRoutes) {
+                      let requestEntityUrl = `${apiEntityRoutes[value]}${index}`;
+
+                      this.getEntityName(requestEntityUrl).then(entityName => {
+                        if (entityName) {
+                          console.log(
+                            `${value} - Retrieved entity name: ${entityName}`
+                          );
+
+                          // Return the Link with the entity name.
+                          return (
+                            // Include key?
+                            <Link color="inherit" href={to}>
+                              {entityName}
+                            </Link>
+                          );
+                        }
+                      });
+                    }
+
+                    return <Link key="">N/A</Link>;
+
+                    // return last ? (
+                    //  <Typography color="textPrimary" key={to}>
+                    //    {value}
+                    //  </Typography>
+                    // ) : (
+
+                    // return (
+                    // <Link color="inherit" href={to} key={to}>
+                    //   {value}
+                    // </Link>
+                    // );
+                    // );
+                  })}
+                </Breadcrumbs>
+              );
+            }}
+          </Route>
+        </Paper>
+      </div>
+    );
   }
 }
 
-
-const mapStateToProps = ((state: StateType, ownProps: { location: Location }): BreadcrumbProps => {
+const mapStateToProps = (state: StateType): { apiUrl: string } => {
+  console.log('apiUrl prop: ', state.dgtable.urls.apiUrl);
   return {
     apiUrl: state.dgtable.urls.apiUrl,
-    location: ownProps.location,
-  }
-});
+  };
+};
 
-export default connect(
-  mapStateToProps,
-)(PageBreadcrumbs);
+export default withRouter(connect(mapStateToProps)(PageBreadcrumbs));
