6a8a1a6e7c9f22e045f5e74538b6ccbecf3c820d
Louise Davies <louise.davies@stfc.ac.uk>
2019-10-23 09:33:38 +0100

    #39 - use settings file for URLs

--- packages/datagateway-table/server/e2e-test-server.js
+++ packages/datagateway-table/server/e2e-test-server.js
@@ -1,39 +1,21 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var axios = require('axios');
 
 var app = express();
+
+app.get('/settings.json', function(req, res) {
+  res.sendFile(path.resolve('./server/e2e-settings.json'));
+});
 
 app.use(
   express.json(),
   serveStatic(path.resolve('./build'), { index: ['index.html', 'index.htm'] })
 );
 
-app.get(/\/sessions|investigations|datasets|datafiles/, function(req, res) {
-  axios
-    .get('http://localhost:5000' + req.url, {
-      headers: req.headers,
-    })
-    .then(apiRes => {
-      res.json(apiRes.data);
-    })
-    .catch(error => {
-      res.status(error.response.status).end();
-    });
-});
-
-app.post(/\/sessions|investigations|datasets|datafiles/, function(req, res) {
-  axios
-    .post('http://localhost:5000' + req.url, req.body, {
-      headers: req.headers,
-    })
-    .then(apiRes => {
-      res.json(apiRes.data);
-    })
-    .catch(error => {
-      res.status(error.response.status).end();
-    });
+// TODO: remove when preloader exists
+app.get(/\/investigations|datasets|datafiles/, function(req, res) {
+  res.json([]);
 });
 
 app.get('/*', function(req, res) {
