df2503229fb9adce1a1423fdf567bd3d9a9376ac
Goel Biju <goelmolel@hotmail.com>
2019-11-01 15:54:46 +0000

    Added DLS datafiles route snapshot test (#51)

--- packages/datagateway-table/src/breadcrumbs.component.test.tsx
+++ packages/datagateway-table/src/breadcrumbs.component.test.tsx
@@ -245,4 +245,19 @@ describe('PageBreadcrumbs - Snapshot Tests (Generic, ISIS, DLS)', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('renders correctly for DLS datafiles route', async () => {
+    // Set up test state pathname.
+    state.router.location = createLocation(DLSRoutes['datafiles']);
+
+    // Set up store with test state and mount the breadcrumb.
+    console.log('Test state: ', state);
+    const wrapper = createWrapper(state);
+
+    // Flush promises and update the re-render the wrapper.
+    await flushPromises();
+    wrapper.update();
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
