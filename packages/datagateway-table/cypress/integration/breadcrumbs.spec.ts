e7f30b66d432d7382dc4906f64478f363b67804c
Goel Biju <goelmolel@hotmail.com>
2019-11-04 16:06:51 +0000

    Added e2e test to view investigation and check breadcrumb trail (#51)

--- packages/datagateway-table/cypress/integration/breadcrumbs.spec.ts
+++ packages/datagateway-table/cypress/integration/breadcrumbs.spec.ts
@@ -1,5 +1,10 @@
 describe('Breadcrumbs Component', () => {
   beforeEach(() => {
+    // Create route and aliases.
+    cy.server();
+    cy.route('/investigations/*').as('getInvestigation');
+
+    // Get a session and visit the table.
     cy.login('user', 'password');
     cy.visit('/browse/investigation');
   });
@@ -7,6 +12,41 @@ describe('Breadcrumbs Component', () => {
   it('should load correctly and display current breadcrumb', () => {
     cy.title().should('equal', 'DataGateway Table');
 
+    // Check that the base Investigations breadcrumb exists.
     cy.contains('Investigations');
+  });
+
+  it('should click on investigation and add breadcrumbs correctly', () => {
+    // Click on an investigation in the table.
+    cy.get('[role="gridcell"] a')
+      .first()
+      .click();
+
+    // Wait for the investigation request to complete.
+    cy.wait('@getInvestigation');
+
+    // Check to see if the breadcrumbs have been added correctly.
+    // Check that there is a link to /browse/investigations.
+    cy.get('a')
+      .first()
+      .contains('Investigations')
+      .should('have.attr', 'href', '/browse/investigation');
+
+    // Get the investigation name.
+    cy.get(':nth-child(3) > .MuiTypography-root').contains(
+      'quas accusantium omnis'
+    );
+
+    // Ensure current page is datasets.
+    cy.get('i')
+      .first()
+      .contains('Datasets');
+  });
+
+  it('loads the breadcrumb trail when visiting a page directly', () => {
+    // Visit a direct link.
+    cy.visit('/browse/investigation/1/dataset/1/datafile');
+
+    cy.wait('@getInvestigation');
   });
 });
