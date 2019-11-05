d129d075781a270bd4b4229786f842ed616a3d65
Goel Biju <goelmolel@hotmail.com>
2019-11-05 10:48:31 +0000

    Added remaining e2e tests for breadcrumb component (#51)

--- packages/datagateway-table/cypress/integration/breadcrumbs.spec.ts
+++ packages/datagateway-table/cypress/integration/breadcrumbs.spec.ts
@@ -3,6 +3,7 @@ describe('Breadcrumbs Component', () => {
     // Create route and aliases.
     cy.server();
     cy.route('/investigations/*').as('getInvestigation');
+    cy.route('/datasets/*').as('getDataset');
 
     // Get a session and visit the table.
     cy.login('user', 'password');
@@ -47,6 +48,80 @@ describe('Breadcrumbs Component', () => {
     // Visit a direct link.
     cy.visit('/browse/investigation/1/dataset/1/datafile');
 
+    // Wait for all API requests to return.
     cy.wait('@getInvestigation');
+    cy.wait('@getDataset');
+
+    // Get Investigations table breadcrumb link.
+    cy.get('a')
+      .first()
+      .contains('Investigations')
+      .should('have.attr', 'href', '/browse/investigation');
+
+    // Get investigation datasets table breadcrumb.
+    cy.get(':nth-child(3) > .MuiTypography-root')
+      .contains('quas accusantium omnis')
+      .should('have.attr', 'href', '/browse/investigation/1/dataset');
+
+    // Get current dataset page breadcrumb.
+    cy.get(':nth-child(5) > .MuiTypography-root').contains('Dataset 1');
+
+    // Get page contents breadcrumb.
+    cy.get('i')
+      .first()
+      .contains('Datafiles');
+  });
+
+  it('loads breadcrumb trail correctly after pressing forward/backward browser keys', () => {
+    // Load pages into the history.
+    cy.visit('/browse/investigation/1/dataset');
+
+    cy.wait('@getInvestigation');
+
+    // Click on the first link with Dataset 1.
+    cy.get('a')
+      .contains('Dataset 1')
+      .click();
+
+    // Check to ensure the location is the datafile.
+    cy.location('pathname').should(
+      'eq',
+      '/browse/investigation/1/dataset/1/datafile'
+    );
+
+    cy.wait('@getDataset');
+
+    // Check the breadcrumb loaded on the current page.
+    cy.get('a')
+      .first()
+      .contains('Investigations')
+      .should('have.attr', 'href', '/browse/investigation');
+
+    cy.get(':nth-child(3) > .MuiTypography-root')
+      .contains('quas accusantium omnis')
+      .should('have.attr', 'href', '/browse/investigation/1/dataset');
+
+    cy.get(':nth-child(5) > .MuiTypography-root').contains('Dataset 1');
+
+    cy.get('i')
+      .first()
+      .contains('Datafiles');
+
+    // Press back.
+    cy.go('back');
+
+    // Check the breadcrumb trail on the previous page.
+    cy.get('a')
+      .first()
+      .contains('Investigations')
+      .should('have.attr', 'href', '/browse/investigation');
+
+    cy.get(':nth-child(3) > .MuiTypography-root').contains(
+      'quas accusantium omnis'
+    );
+
+    cy.get('i')
+      .first()
+      .contains('Datasets');
   });
 });
