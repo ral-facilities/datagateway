ef8925525c365451b0ebacb5f6830cc270b07cb7
Goel Biju <goelmolel@hotmail.com>
2019-11-06 13:34:00 +0000

    Added sort by test for DLSProposals Table (#23)

--- packages/datagateway-table/cypress/integration/dls/proposals.spec.ts
+++ packages/datagateway-table/cypress/integration/dls/proposals.spec.ts
@@ -18,4 +18,37 @@ describe('Proposals Table', () => {
       '/browse/proposal/INVESTIGATION%201/investigation/'
     );
   });
+
+  it('should be able to scroll down and load more rows', () => {
+    cy.get('[aria-rowcount="50"]').should('exist');
+    cy.get('[aria-label="grid"]').scrollTo('bottom');
+    cy.get('[aria-rowcount="75"]').should('exist');
+  });
+
+  describe('should be able to sort by', () => {
+    it('ascending order', () => {
+      cy.contains('Title').click();
+
+      cy.get('[aria-sort="ascending"]').should('exist');
+      cy.get('.MuiTableSortLabel-iconDirectionAsc').should('be.visible');
+      cy.get('[aria-rowindex="1"] [aria-colindex="1"]').contains(
+        'A nothing almost arrive I. Product middle design never. Cup camera then product father sort vote.'
+      );
+    });
+
+    it.only('descending order', () => {
+      cy.contains('Title').click();
+      cy.contains('Title').click();
+
+      cy.get('[aria-sort="descending"]').should('exist');
+      cy.get('.MuiTableSortLabel-iconDirectionDesc').should(
+        'not.have.css',
+        'opacity',
+        '0'
+      );
+      cy.get('[aria-rowindex="1"] [aria-colindex="1"]').contains(
+        'Whom anything affect consider left. Entire order tough. White responsibility economic travel activity.'
+      );
+    });
+  });
 });
