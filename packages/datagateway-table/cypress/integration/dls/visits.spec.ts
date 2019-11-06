e4b2dfd28b57e698227984f4c3e69094de554a3f
Goel Biju <goelmolel@hotmail.com>
2019-11-06 15:04:17 +0000

    Added 'sort by' e2e tests for DLS Visits Table (#23)

--- packages/datagateway-table/cypress/integration/dls/visits.spec.ts
+++ packages/datagateway-table/cypress/integration/dls/visits.spec.ts
@@ -9,7 +9,7 @@ describe('DLS - Visits Table', () => {
     cy.get('#datagateway-table').should('be.visible');
   });
 
-  it.only('should be able to click an investigation to see its datasets', () => {
+  it('should be able to click an investigation to see its datasets', () => {
     cy.get('[role="gridcell"] a')
       .first()
       .click({ force: true });
@@ -17,5 +17,51 @@ describe('DLS - Visits Table', () => {
       'eq',
       '/browse/proposal/INVESTIGATION%201/investigation/1/dataset'
     );
+  });
+
+  describe('should be able to sort by', () => {
+    it('ascending order', () => {
+      cy.contains('Visit Id').click();
+
+      cy.get('[aria-sort="ascending"]').should('exist');
+      cy.get('.MuiTableSortLabel-iconDirectionAsc').should('be.visible');
+      cy.get('[aria-rowindex="1"] [aria-colindex="2"]').contains('64');
+    });
+
+    it('descending order', () => {
+      cy.contains('Visit Id').click();
+      cy.contains('Visit Id').click();
+
+      cy.get('[aria-sort="descending"]').should('exist');
+      cy.get('.MuiTableSortLabel-iconDirectionDesc').should(
+        'not.have.css',
+        'opacity',
+        '0'
+      );
+      cy.get('[aria-rowindex="1"] [aria-colindex="2"]').contains('64');
+    });
+
+    it('no order', () => {
+      cy.contains('Visit Id').click();
+      cy.contains('Visit Id').click();
+      cy.contains('Visit Id').click();
+
+      cy.get('[aria-sort="ascending"]').should('not.exist');
+      cy.get('[aria-sort="descending"]').should('not.exist');
+      cy.get('.MuiTableSortLabel-iconDirectionAsc').should('not.be.visible');
+      cy.get('.MuiTableSortLabel-iconDirectionDesc').should(
+        'have.css',
+        'opacity',
+        '0'
+      );
+      cy.get('[aria-rowindex="1"] [aria-colindex="2"]').contains('64');
+    });
+
+    it('multiple columns', () => {
+      cy.contains('Start Date').click();
+      cy.contains('Visit Id').click();
+
+      cy.get('[aria-rowindex="1"] [aria-colindex="2"]').contains('64');
+    });
   });
 });
