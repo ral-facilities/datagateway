ab418385012959bde708601810776bf325ab2c2c
Louise Davies <louise.davies@stfc.ac.uk>
2019-12-05 17:14:23 +0000

    Add cypress retries package to automatically retry tests in CI

--- packages/datagateway-download/cypress/plugins/index.js
+++ packages/datagateway-download/cypress/plugins/index.js
@@ -15,9 +15,9 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require('cypress-plugin-retries/lib/plugin')(on);
   on('task', {
     // it's installed at the top level, so disable the check
-    // eslint-disable-next-line import/no-extraneous-dependencies
     failed: require('cypress-failed-log/src/failed')(),
   });
 };
