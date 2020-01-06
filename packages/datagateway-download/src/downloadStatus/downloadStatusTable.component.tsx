d73281fbd3a78689439f2307e7ff28174a9ea29b
Goel Biju <goelmolel@hotmail.com>
2020-01-06 10:41:45 +0000

    Created initial DownloadStatusTable (#48)

---
+++ packages/datagateway-download/src/downloadStatus/downloadStatusTable.component.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+// import { TextColumnFilter } from 'datagateway-common';
+
+const DownloadStatusTable: React.FC = () => {
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        {/* <Paper style={{ height: 'calc(100vh - 150px)' }}>
+                    <Table
+                        columns={[
+                            {
+                                label: 'Download Name',
+                                dataKey: 'downloadName',
+                                filterComponent: textFilter,
+                            },
+                            {
+                                label: 'Access Method',
+                                
+                            }
+                        ]}
+                    >
+
+                    </Table>
+                </Paper> */}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default DownloadStatusTable;
