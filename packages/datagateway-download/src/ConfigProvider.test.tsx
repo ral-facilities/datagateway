b95d6c34218a3896e34a208ff172ac664f5111ad
Goel Biju <goelmolel@hotmail.com>
2020-02-23 23:16:54 +0000

    Add facilityName unit test for ConfigProvider

--- packages/datagateway-download/src/ConfigProvider.test.tsx
+++ packages/datagateway-download/src/ConfigProvider.test.tsx
@@ -1,6 +1,7 @@
 import { createMount } from '@material-ui/core/test-utils';
 import axios from 'axios';
 import { ReactWrapper } from 'enzyme';
+import * as log from 'loglevel';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import ConfigProvider, { DownloadSettingsContext } from './ConfigProvider';
@@ -38,7 +39,7 @@ describe('ConfigProvider', () => {
     );
   };
 
-  it.only('settings (facilityName, URLs and accessMethods) are loaded', async () => {
+  it('settings (facilityName, URLs and accessMethods) are loaded', async () => {
     const settingsResult = {
       facilityName: 'Generic',
       idsUrl: 'ids',
@@ -72,7 +73,45 @@ describe('ConfigProvider', () => {
     );
   });
 
-  it('logs an error if facilityName is not defined in the settings');
+  it.only('logs an error if facilityName is not defined in the settings', async () => {
+    const settingsResult = {
+      idsUrl: 'ids',
+      apiUrl: 'api',
+      downloadApiUrl: 'download-api',
+      accessMethods: {
+        https: {
+          idsUrl: 'https-ids',
+          displayName: 'HTTPS',
+          description: 'HTTP description',
+        },
+      },
+    };
+
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({
+        data: settingsResult,
+      })
+    );
+
+    // Create the wrapper and wait for it to load.
+    const wrapper = createWrapper();
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    console.log(wrapper.find('#settings'));
+    // Expect the wrapper to be empty (with no settings)
+    // and the appropriate error to have been logged.
+    expect(wrapper.exists('#settings')).toBe(false);
+    expect(log.error).toHaveBeenCalled();
+
+    const mockLog = (log.error as jest.Mock).mock;
+    expect(mockLog.calls[0][0]).toEqual(
+      'Error loading datagateway-download-settings.json: facilityName is undefined in settings'
+    );
+  });
+
   it('logs an error if any of the API URLs are not defined in the settings');
   it(
     'logs an error if fails to load a settings.json and is still in a loading state'
