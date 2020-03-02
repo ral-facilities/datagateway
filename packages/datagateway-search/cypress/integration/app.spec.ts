dac27d9e487fbc1e1de03a866d3cf4f13960c98e
Emily <emzlewis@hotmail.co.uk>
2020-03-02 16:11:00 +0000

    Removed the e2e test breaking the build #33

--- packages/datagateway-search/cypress/integration/app.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('App', () => {
-  it('should load correctly', () => {
-    cy.visit('/');
-    cy.title().should('equal', 'DataGateway Search');
-
-    cy.get('#datagateway-search').should('be.visible');
-  });
-});
