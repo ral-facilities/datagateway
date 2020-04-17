3950fdede44e52cb52b2b54e0a97b731adeb96e7
Goel Biju <goelmolel@hotmail.com>
2020-04-17 10:21:30 +0100

    Split cardView to separate components (#128)

---
+++ packages/datagateway-table/src/card/card.component.tsx
@@ -0,0 +1,327 @@
+import React from 'react';
+
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Chip,
+  Divider,
+  Button,
+  Collapse,
+  Link,
+} from '@material-ui/core';
+import {
+  AddCircleOutlineOutlined,
+  RemoveCircleOutlineOutlined,
+} from '@material-ui/icons';
+
+import ArrowTooltip from '../page/arrowtooltip.component';
+
+// TODO: Understand CSS flexbox to style this correctly OR use Grid/GridList instead of Card?
+const useCardStyles = makeStyles((theme: Theme) => {
+  // NOTE: This is width of the main content
+  //       (this also matches the description shadow width).
+  const mainWidth = '35vw';
+
+  const styles = createStyles({
+    root: {
+      display: 'flex',
+      maxWidth: 1000,
+      backgroundColor: theme.palette.background.paper,
+    },
+
+    // TODO: Automatically size to card size?
+    cardImage: {
+      width: 150,
+      height: 150,
+    },
+
+    content: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+
+    main: {
+      display: 'flex',
+      // Have contents arranged in columns.
+      flexDirection: 'column',
+
+      // NOTE: You will also have to change the
+      // This is the width of the entire container
+      // (so title won't exceed 30% of the viewport width).
+      width: mainWidth,
+      paddingRight: '10px',
+    },
+
+    // NOTE: Styling specifically for the title as we want
+    //       the text to take up only the width it needs so we
+    //       know when to show the arrow toolip when the text has
+    //       overflowed the maximum width given for the title.
+    title: {
+      display: 'inline-block',
+      // whiteSpace: 'nowrap',
+      maxWidth: '100%',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+    },
+
+    description: {
+      '& a': {
+        cursor: 'pointer',
+      },
+    },
+
+    shadowVisible: {
+      position: 'absolute',
+      height: 25,
+      width: mainWidth,
+      top: 130,
+      background: 'linear-gradient(rgba(255, 255, 255, 0), #fff)',
+
+      // Transition showing the shadow.
+      visibility: 'visible',
+      opacity: 1,
+      transition: 'visibility 0s, opacity 0.5s linear',
+    },
+
+    shadowInvisible: {
+      visibility: 'hidden',
+      opacity: 0,
+      transition: 'visibility 0s, opacity 0.5s linear',
+    },
+
+    further: {
+      display: 'flex',
+      paddingLeft: '15px',
+
+      // Apply a small space for each Typography component (p).
+      '& p': {
+        paddingTop: '5px',
+      },
+    },
+
+    tags: {
+      paddingTop: '10px',
+    },
+
+    chip: {
+      margin: theme.spacing(0.5),
+    },
+  });
+  return styles;
+});
+
+interface EntityCardProps {
+  title: string;
+  summary?: string;
+  startDate?: string;
+  endDate?: string;
+
+  doi?: string;
+  visitId: string;
+  datasetCount?: number;
+
+  // TODO: optional and no information to create these
+  imageUrl?: string;
+  tags?: typeof Chip[];
+}
+
+const EntityCard = (props: EntityCardProps): React.ReactElement => {
+  const classes = useCardStyles();
+  const {
+    title,
+    summary,
+    startDate,
+    endDate,
+    doi,
+    visitId,
+    datasetCount,
+
+    imageUrl,
+    tags,
+  } = props;
+
+  const [isSelected, setIsSelected] = React.useState(false);
+
+  // The default collapsed height for card description is 100px.
+  const defaultCollapsedHeight = 100;
+  const [isCollapsed, setIsCollapsed] = React.useState(false);
+  const descriptionRef = React.useRef<HTMLParagraphElement>(null);
+  const [collapsibleInteraction, setCollapsibleInteraction] = React.useState(
+    false
+  );
+
+  React.useEffect(() => {
+    if (descriptionRef && descriptionRef.current) {
+      // console.log('Description height: ', descriptionRef.current.clientHeight);
+      if (descriptionRef.current.clientHeight > defaultCollapsedHeight)
+        setCollapsibleInteraction(true);
+    }
+  }, [setCollapsibleInteraction]);
+
+  return (
+    <Card id="card" className={classes.root}>
+      {imageUrl && (
+        <CardMedia
+          component="img"
+          className={classes.cardImage}
+          // TODO: Test image.
+          // image="https://www.iconbolt.com/iconsets/streamline-regular/lab-flask-experiment.svg"
+          image={imageUrl}
+          title="Investigation/Dataset image"
+        />
+      )}
+      {/* TODO: Card content needs to be a flexbox (as a row):
+                - has a card information area (split in horizontally - column) for title/description and tags
+                - has card details area which takes up smaller space */}
+      <CardContent className={classes.content}>
+        {/* row:
+                - main information
+                - further information; dates, DOI, visit id
+                 */}
+        <div className={classes.main}>
+          {/*column:
+                - title/description
+                - tags  */}
+          <div>
+            {/* TODO: Title needs to be cut off if it breaks the set width of the column
+                    Show it in a tooltip as well */}
+            {/* TODO: The title needs to link to the next entity (investigation/dataset) - or have it as the whole card? */}
+            {/* TODO: Delay not consistent between cards? */}
+            {/* TODO: Any calculations of width/height (offset) done by the arrow
+                toolip begins with with the first wrapping element in the tooltip.
+                This is the Typography component in our case. */}
+            <ArrowTooltip
+              title={title}
+              enterDelay={500}
+              // TODO: Move to different component.
+              // disableFocusListener={isCollapsed}
+              percentageWidth={30}
+              maxEnabledHeight={32}
+            >
+              <Typography className={classes.title} component="h5" variant="h5">
+                <span style={{ whiteSpace: isCollapsed ? 'normal' : 'nowrap' }}>
+                  {title}
+                </span>
+              </Typography>
+            </ArrowTooltip>
+
+            {/* TODO: Maybe include option to have read more if description is too long? 
+                        Similar to collapsible 
+              */}
+            <div className={classes.description}>
+              {/* TODO: collapsedHeight being the minimum description content to
+                show for each card. */}
+              <Collapse
+                in={isCollapsed}
+                collapsedHeight={defaultCollapsedHeight}
+              >
+                <Typography ref={descriptionRef} variant="body1" paragraph>
+                  {summary ? summary : 'No description available'}
+                  {/* Inhabiting discretion the her dispatched decisively boisterous
+                    joy. So form were wish open is able of mile of. Waiting
+                    express if prevent it we an musical. Especially reasonable
+                    travelling she son. Resources resembled forfeited no to
+                    zealously. Has procured daughter how friendly followed
+                    repeated who surprise. Great asked oh under on voice downs.
+                    Preference connection astonished on of ye. Partiality on or
+                    continuing in particular principles as. Do believing oh
+                    disposing to supported allowance we. */}
+                </Typography>
+              </Collapse>
+
+              {collapsibleInteraction && (
+                <div>
+                  <div
+                    className={
+                      isCollapsed
+                        ? classes.shadowInvisible
+                        : classes.shadowVisible
+                    }
+                  />
+                  <Link onClick={() => setIsCollapsed(prev => !prev)}>
+                    {isCollapsed ? 'Show less' : 'Show more'}
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {tags && (
+            <div className={classes.tags}>
+              <Divider />
+
+              {/* TODO: Maybe this should be an array of tags? What would these tags be based on? */}
+              <div style={{ paddingTop: '10px' }}>
+                <Chip className={classes.chip} label="particle" size="small" />
+                <Chip
+                  className={classes.chip}
+                  label="experiment"
+                  size="small"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+
+        <Divider orientation={'vertical'} />
+
+        {/* TODO: Add in margins for spacing. */}
+        {/* TODO: These should be specified elsewhere */}
+        <div>
+          <div className={classes.further}>
+            {/* TODO: Further information related to the entity. */}
+            <div style={{ float: 'left' }}>
+              <Typography>Start Date:</Typography>
+              <Typography>End Date:</Typography>
+              <Typography>DOI:</Typography>
+              <Typography>Visit ID:</Typography>
+
+              {datasetCount && <Typography>Dataset Count:</Typography>}
+            </div>
+            <div
+              style={{ float: 'right', textAlign: 'left', paddingLeft: '5px' }}
+            >
+              <Typography>{startDate}</Typography>
+              <Typography>{endDate}</Typography>
+              <Typography>{doi}</Typography>
+              <Typography>{visitId}</Typography>
+              {datasetCount && <Typography>{datasetCount}</Typography>}
+            </div>
+          </div>
+
+          {/* TODO: Add to cart button - requires API logic */}
+          <div style={{ paddingTop: '15px', textAlign: 'center' }}>
+            {!isSelected ? (
+              <Button
+                id="add-to-cart-btn"
+                variant="contained"
+                color="primary"
+                startIcon={<AddCircleOutlineOutlined />}
+                disableElevation
+                onClick={() => setIsSelected(true)}
+              >
+                Add to cart
+              </Button>
+            ) : (
+              <Button
+                id="remove-from-cart-btn"
+                variant="contained"
+                color="secondary"
+                startIcon={<RemoveCircleOutlineOutlined />}
+                disableElevation
+                onClick={() => setIsSelected(false)}
+              >
+                Remove from cart
+              </Button>
+            )}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default EntityCard;
