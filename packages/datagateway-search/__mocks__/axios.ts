0740bb5ecf00a07cf3831cf7dbc0acbf827991ac
Louise Davies <louise.davies@stfc.ac.uk>
2020-05-07 14:33:09 +0100

    Remove warnings by adding missing axios mock method

--- packages/datagateway-search/__mocks__/axios.ts
+++ packages/datagateway-search/__mocks__/axios.ts
@@ -3,5 +3,6 @@ import axios from 'axios';
 export default {
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} })),
   CancelToken: axios.CancelToken,
 };
