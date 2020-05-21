2a2fbd648da75c89a66dda7a0186f559e27bc39e
Louise Davies <louise.davies@stfc.ac.uk>
2020-05-21 15:03:55 +0100

    Move __mocks__ folders to fix unit tests

--- packages/datagateway-dataview/src/__mocks__/axios.ts
+++ packages/datagateway-dataview/src/__mocks__/axios.ts
@@ -1,3 +1,5 @@
+// TODO: move __mocks__ folder back to package root once facebook/create-react-app#7539 is fixed
+
 import axios from 'axios';
 
 export default {
--- packages/datagateway-search/src/__mocks__/axios.ts
+++ packages/datagateway-search/src/__mocks__/axios.ts
@@ -1,8 +1,9 @@
+// TODO: move __mocks__ folder back to package root once facebook/create-react-app#7539 is fixed
+
 import axios from 'axios';
 
 export default {
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
-  delete: jest.fn(() => Promise.resolve({ data: {} })),
   CancelToken: axios.CancelToken,
 };
