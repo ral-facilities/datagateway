e578cac426e42ba2c8dac206f31e83422e58f31f
Louise Davies <louise.davies@stfc.ac.uk>
2020-05-21 16:19:04 +0100

    Type lodash.debounce mock

--- packages/datagateway-dataview/src/__mocks__/lodash.debounce.ts
+++ packages/datagateway-dataview/src/__mocks__/lodash.debounce.ts
@@ -1,3 +1,3 @@
 // TODO: move __mocks__ folder back to package root once facebook/create-react-app#7539 is fixed
 
-export default () => fn => fn;
+export default <A, T>() => (fn: (args: A) => T) => fn;
