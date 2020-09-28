3b141443072002495f10bce11e152d679d308d9e
Patrick Austin <patrick.austin@stfc.ac.uk>
2020-09-28 14:11:42 +0100

    justify centre and remove advance search toggle #416

--- packages/datagateway-search/cypress/integration/searchBoxContainer.spec.ts
+++ packages/datagateway-search/cypress/integration/searchBoxContainer.spec.ts
@@ -12,32 +12,13 @@ describe('SearchBoxContainer Component', () => {
 
     cy.get('[aria-label="Search text input"]').should('exist');
 
-    cy.get('[aria-label="Start date input"]').should('not.exist');
-    cy.get('[aria-label="End date input"]').should('not.exist');
-
-    cy.get('[aria-label="Investigation checkbox"]').should('not.exist');
-    cy.get('[aria-label="Dataset checkbox"]').should('not.exist');
-    cy.get('[aria-label="Datafile checkbox"]').should('not.exist');
-
-    cy.get('[aria-label="Submit search button"]').should('exist');
-    cy.get('[aria-label="Toggle advanced search"]').should('exist');
-  });
-
-  it('display advanced search settings on toggle', () => {
-    cy.get('[aria-label="Toggle advanced search"]').click();
-
     cy.get('[aria-label="Start date input"]').should('exist');
     cy.get('[aria-label="End date input"]').should('exist');
+
     cy.get('[aria-label="Investigation checkbox"]').should('exist');
     cy.get('[aria-label="Dataset checkbox"]').should('exist');
     cy.get('[aria-label="Datafile checkbox"]').should('exist');
 
-    cy.get('[aria-label="Toggle advanced search"]').click();
-
-    cy.get('[aria-label="Start date input"]').should('not.exist');
-    cy.get('[aria-label="End date input"]').should('not.exist');
-    cy.get('[aria-label="Investigation checkbox"]').should('not.exist');
-    cy.get('[aria-label="Dataset checkbox"]').should('not.exist');
-    cy.get('[aria-label="Datafile checkbox"]').should('not.exist');
+    cy.get('[aria-label="Submit search button"]').should('exist');
   });
 });
--- packages/datagateway-search/src/searchBoxContainer.component.tsx
+++ packages/datagateway-search/src/searchBoxContainer.component.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { Grid, Box, IconButton } from '@material-ui/core';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import { Grid, Box } from '@material-ui/core';
 
 import SelectDates from './search/datePicker.component';
 import CheckboxesGroup from './search/checkBoxes.component';
@@ -10,32 +8,15 @@ import SearchButton from './search/searchButton.component';
 import SearchTextBox from './search/searchTextBox.component';
 
 const SearchBoxContainer = (): React.ReactElement => {
-  const [advancedSearch, setAdvancedSearch] = React.useState(false);
-
   return (
     <Grid
       container
       direction="row"
-      justify="flex-start"
+      justify="center"
       alignItems="stretch"
       id="container-searchbox"
     >
-      <Grid item style={{ display: 'flex' }}>
-        <Box pl={2} m="auto">
-          <IconButton
-            aria-label="Toggle advanced search"
-            onClick={() => setAdvancedSearch(!advancedSearch)}
-          >
-            {advancedSearch ? (
-              <KeyboardArrowDownIcon />
-            ) : (
-              <KeyboardArrowRightIcon />
-            )}
-          </IconButton>
-        </Box>
-      </Grid>
-
-      <Grid item xs>
+      <Grid item xs={8}>
         <Box pl={2} pb={1}>
           <SearchTextBox />
         </Box>
@@ -47,23 +28,19 @@ const SearchBoxContainer = (): React.ReactElement => {
         </Box>
       </Grid>
 
-      {advancedSearch ? (
-        <div>
-          <Grid container item style={{ paddingBottom: 8 }}>
-            <Grid item style={{ display: 'flex' }}>
-              <Box m="auto">
-                <CheckboxesGroup />
-              </Box>
-            </Grid>
+      <Grid container item justify="center" style={{ paddingBottom: 8 }}>
+        <Grid item style={{ display: 'flex' }}>
+          <Box m="auto">
+            <CheckboxesGroup />
+          </Box>
+        </Grid>
 
-            <Grid item>
-              <Box px={2}>
-                <SelectDates />
-              </Box>
-            </Grid>
-          </Grid>
-        </div>
-      ) : null}
+        <Grid item>
+          <Box px={2}>
+            <SelectDates />
+          </Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
