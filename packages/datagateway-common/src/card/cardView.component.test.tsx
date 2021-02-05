5d944257dcc3e5671af9413cd5fc28e3a78f2adf
Patrick Austin <patrick.austin@stfc.ac.uk>
2021-02-05 11:25:12 +0000

    Increase line coverage in cardView.component.test #461

--- packages/datagateway-common/src/card/cardView.component.test.tsx
+++ packages/datagateway-common/src/card/cardView.component.test.tsx
@@ -3,7 +3,9 @@ import {
   Chip,
   ExpansionPanel,
   ListItemText,
+  Select,
   SvgIcon,
+  Typography,
 } from '@material-ui/core';
 import { createMount, createShallow } from '@material-ui/core/test-utils';
 import { Pagination } from '@material-ui/lab';
@@ -191,6 +193,18 @@ describe('Card View', () => {
     expect(wrapper.exists(AdvancedFilter)).toBeTruthy();
   });
 
+  it('filter message displayed when loadedData and totalDataCount is 0', () => {
+    const updatedProps = {
+      ...props,
+      loadedData: true,
+      totalDataCount: 0,
+    };
+    const wrapper = createWrapper(updatedProps);
+    expect(wrapper.find(Typography).last().text()).toEqual(
+      'loading.filter_message'
+    );
+  });
+
   it('buttons display correctly', () => {
     const updatedProps = {
       ...props,
@@ -200,31 +214,26 @@ describe('Card View', () => {
     expect(wrapper.find('#test-button').first().text()).toEqual('TEST');
   });
 
-  it('information displays correctly', () => {
+  it('moreInformation displays correctly', () => {
+    const moreInformation = (entity: Entity): React.ReactElement => <p>TEST</p>;
     const updatedProps = {
       ...props,
-      information: [
-        { dataKey: 'VISIT_ID' },
-        {
-          dataKey: 'NAME',
-          label: 'Name',
-          content: (entity: Entity) => 'Content',
-        },
-      ],
+      moreInformation: moreInformation,
     };
     const wrapper = createWrapper(updatedProps);
-    expect(
-      wrapper.find('[aria-label="card-info-VISIT_ID"]').first().text()
-    ).toEqual('VISIT_ID:');
-    expect(
-      wrapper.find('[aria-label="card-info-data-VISIT_ID"]').first().text()
-    ).toEqual('1');
-    expect(
-      wrapper.find('[aria-label="card-info-Name"]').first().text()
-    ).toEqual('Name:');
-    expect(
-      wrapper.find('[aria-label="card-info-data-Name"]').first().text()
-    ).toEqual('Content');
+    expect(wrapper.exists('[aria-label="card-more-information"]')).toBeTruthy();
+  });
+
+  it('title.content displays correctly', () => {
+    const content = (entity: Entity): React.ReactElement => (
+      <p id="test-title-content">TEST</p>
+    );
+    const updatedProps = {
+      ...props,
+      title: { dataKey: 'TITLE', content: content },
+    };
+    const wrapper = createWrapper(updatedProps);
+    expect(wrapper.find('#test-title-content').text()).toEqual('TEST');
   });
 
   it('sort applied correctly', () => {
@@ -351,12 +360,76 @@ describe('Card View', () => {
     });
   });
 
+  it('information displays and sorts correctly', () => {
+    let updatedProps = {
+      ...props,
+      title: { dataKey: 'TITLE', disableSort: true },
+      description: { dataKey: 'NAME', disableSort: true },
+      information: [
+        { dataKey: 'VISIT_ID' },
+        {
+          dataKey: 'NAME',
+          label: 'Name',
+          content: (entity: Entity) => 'Content',
+        },
+      ],
+      query: { ...props.query, page: 1 },
+    };
+    const wrapper = createWrapper(updatedProps);
+    expect(
+      wrapper.find('[aria-label="card-info-VISIT_ID"]').first().text()
+    ).toEqual('VISIT_ID:');
+    expect(
+      wrapper.find('[aria-label="card-info-data-VISIT_ID"]').first().text()
+    ).toEqual('1');
+    expect(
+      wrapper.find('[aria-label="card-info-Name"]').first().text()
+    ).toEqual('Name:');
+    expect(
+      wrapper.find('[aria-label="card-info-data-Name"]').first().text()
+    ).toEqual('Content');
+
+    // Click to sort ascending
+    const button = wrapper.find(ListItemText).first();
+    expect(button.text()).toEqual('VISIT_ID');
+    button.simulate('click');
+    expect(pushQuery).toHaveBeenNthCalledWith(1, {
+      ...updatedProps.query,
+      sort: { VISIT_ID: 'asc' },
+    });
+    updatedProps = {
+      ...updatedProps,
+      query: { ...updatedProps.query, sort: { VISIT_ID: 'asc' } },
+    };
+    wrapper.setProps(updatedProps);
+
+    // Click to sort descending
+    button.simulate('click');
+    expect(pushQuery).toHaveBeenNthCalledWith(2, {
+      ...updatedProps.query,
+      sort: { VISIT_ID: 'desc' },
+    });
+    updatedProps = {
+      ...updatedProps,
+      query: { ...updatedProps.query, sort: { VISIT_ID: 'desc' } },
+    };
+    wrapper.setProps(updatedProps);
+
+    // Click to clear sorting
+    button.simulate('click');
+    expect(pushQuery).toHaveBeenNthCalledWith(3, {
+      ...updatedProps.query,
+      sort: {},
+    });
+  });
+
   it('cannot sort when fields are disabled', () => {
     const updatedProps = {
       ...props,
       query: { ...props.query, page: 1 },
       title: { dataKey: 'TITLE', disableSort: true },
       description: { dataKey: 'NAME', disableSort: true },
+      information: [{ dataKey: 'VISIT_ID', disableSort: true }],
     };
     const wrapper = createWrapper(updatedProps);
     expect(wrapper.exists(ListItemText)).toBeFalsy();
@@ -429,6 +502,118 @@ describe('Card View', () => {
     expect(loadData).toHaveBeenNthCalledWith(1, {
       startIndex: 0,
       stopIndex: 0,
+    });
+  });
+
+  it('loadData not called when loadedCount false', () => {
+    const updatedProps = {
+      ...props,
+      data: [
+        {
+          ID: 1,
+          TITLE: 'Test 1',
+          NAME: 'Test 1',
+          VISIT_ID: '1',
+          TYPE_ID: '1',
+          FACILITY_ID: '1',
+        },
+      ],
+      totalDataCount: 1,
+      resultsOptions: [1],
+      loadedCount: false,
+      loadedData: false,
+      query: { ...props.query, results: 1, page: 1 },
+    };
+    createWrapper(updatedProps);
+    expect(loadData).toHaveBeenCalledTimes(0);
+  });
+
+  it('selector sends pushQuery with results', () => {
+    const updatedProps = {
+      ...props,
+      data: [
+        {
+          ID: 1,
+          TITLE: 'Test 1',
+          NAME: 'Test 1',
+          VISIT_ID: '1',
+          TYPE_ID: '1',
+          FACILITY_ID: '1',
+        },
+        {
+          ID: 2,
+          TITLE: 'Test 2',
+          NAME: 'Test 2',
+          VISIT_ID: '2',
+          TYPE_ID: '2',
+          FACILITY_ID: '2',
+        },
+        {
+          ID: 3,
+          TITLE: 'Test 3',
+          NAME: 'Test 3',
+          VISIT_ID: '3',
+          TYPE_ID: '3',
+          FACILITY_ID: '3',
+        },
+      ],
+      totalDataCount: 3,
+      resultsOptions: [1, 2, 3],
+      query: { ...props.query, results: 1, page: 2 },
+    };
+    const wrapper = createWrapper(updatedProps);
+    wrapper
+      .find(Select)
+      .props()
+      .onChange({ target: { value: 2 } });
+    expect(pushQuery).toHaveBeenNthCalledWith(1, {
+      ...updatedProps.query,
+      results: 2,
+    });
+  });
+
+  it('selector sends pushQuery with results and page', () => {
+    const updatedProps = {
+      ...props,
+      data: [
+        {
+          ID: 1,
+          TITLE: 'Test 1',
+          NAME: 'Test 1',
+          VISIT_ID: '1',
+          TYPE_ID: '1',
+          FACILITY_ID: '1',
+        },
+        {
+          ID: 2,
+          TITLE: 'Test 2',
+          NAME: 'Test 2',
+          VISIT_ID: '2',
+          TYPE_ID: '2',
+          FACILITY_ID: '2',
+        },
+        {
+          ID: 3,
+          TITLE: 'Test 3',
+          NAME: 'Test 3',
+          VISIT_ID: '3',
+          TYPE_ID: '3',
+          FACILITY_ID: '3',
+        },
+      ],
+      totalDataCount: 3,
+      resultsOptions: [1, 2, 3],
+      query: { ...props.query, results: 1, page: 2 },
+    };
+    const wrapper = createWrapper(updatedProps);
+    wrapper
+      .find(Select)
+      .props()
+      .onChange({ target: { value: 3 } });
+    expect(pushQuery).toHaveBeenNthCalledWith(1, {
+      ...updatedProps.query,
+      results: 3,
+      page: 1,
     });
   });
 });
