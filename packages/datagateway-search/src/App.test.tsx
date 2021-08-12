34bfe7b00a620cbc73ecda3e04513f64241d13d4
Louise Davies <louise.davies@stfc.ac.uk>
2021-08-12 12:13:09 +0100

    Fix search App test

--- packages/datagateway-search/src/App.test.tsx
+++ packages/datagateway-search/src/App.test.tsx
@@ -34,8 +34,8 @@ describe('App', () => {
 
     expect(log.error).toHaveBeenCalled();
     const mockLog = (log.error as jest.Mock).mock;
-    expect(mockLog.calls[0][0]).toEqual(
-      `datagateway_search failed with error: ${error}`
-    );
+    expect(mockLog.calls).toContainEqual([
+      `datagateway_search failed with error: ${error}`,
+    ]);
   });
 });
