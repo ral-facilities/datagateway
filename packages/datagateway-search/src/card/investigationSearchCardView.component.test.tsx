02f1425c8000f3c03c21a96e39d9a1c0c6f38547
Joel Davies <joel.davies@stfc.ac.uk>
2021-09-22 15:40:27 +0100

    Adding some InvestigationSearchCardView unit tests (#420)

--- packages/datagateway-search/src/card/investigationSearchCardView.component.test.tsx
+++ packages/datagateway-search/src/card/investigationSearchCardView.component.test.tsx
@@ -10,6 +10,7 @@ import {
   useInvestigationSizes,
   Investigation,
   StateType,
+  AdvancedFilter,
 } from 'datagateway-common';
 import InvestigationSearchCardView from './investigationSearchCardView.component';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -21,6 +22,7 @@ import configureStore from 'redux-mock-store';
 
 import { createMemoryHistory, History } from 'history';
 import { initialState as dgSearchInitialState } from '../state/reducers/dgsearch.reducer';
+import { Link, ListItemText } from '@material-ui/core';
 
 jest.mock('datagateway-common', () => {
   const originalModule = jest.requireActual('datagateway-common');
@@ -85,7 +87,10 @@ describe('Investigation - Card View', () => {
       data: 1,
       isLoading: false,
     });
-
+    (useInvestigationsPaginated as jest.Mock).mockReturnValue({
+      data: cardData,
+      isLoading: false,
+    });
     (useLuceneSearch as jest.Mock).mockReturnValue({
       data: [],
     });
@@ -134,13 +139,112 @@ describe('Investigation - Card View', () => {
     expect(wrapper.find('CardView').props()).toMatchSnapshot();
   });
 
-  it.skip('calls the correct data fetching hooks on load');
+  it('calls the correct data fetching hooks on load', () => {
+    (useLuceneSearch as jest.Mock).mockReturnValue({
+      data: [1],
+    });
 
-  it.skip('updates filter query params on text filter');
+    createWrapper();
 
-  it.skip('updates filter query params on date filter');
+    expect(useLuceneSearch).toHaveBeenCalledWith('Investigation', {
+      searchText: state.dgsearch.searchText,
+      startDate: state.dgsearch.selectDate.startDate,
+      endDate: state.dgsearch.selectDate.endDate,
+    });
 
-  it.skip('updates sort query params on sort');
+    expect(useInvestigationCount).toHaveBeenCalledWith([
+      {
+        filterType: 'where',
+        filterValue: JSON.stringify({
+          id: { in: [1] },
+        }),
+      },
+    ]);
+    expect(useInvestigationsPaginated).toHaveBeenCalledWith([
+      {
+        filterType: 'where',
+        filterValue: JSON.stringify({
+          id: { in: [1] },
+        }),
+      },
+      {
+        filterType: 'include',
+        filterValue: JSON.stringify({
+          investigationInstruments: 'instrument',
+        }),
+      },
+    ]);
+    expect(useInvestigationsDatasetCount).toHaveBeenCalledWith(cardData);
+    expect(useInvestigationSizes).toHaveBeenCalledWith([]);
+  });
 
-  it.skip('renders fine with incomplete data');
+  it('updates filter query params on text filter', () => {
+    const wrapper = createWrapper();
+
+    const advancedFilter = wrapper.find(AdvancedFilter);
+    advancedFilter.find(Link).simulate('click');
+    advancedFilter
+      .find('input')
+      .first()
+      .simulate('change', { target: { value: 'test' } });
+
+    expect(history.length).toBe(2);
+    expect(history.location.search).toBe(
+      `?filters=${encodeURIComponent(
+        '{"title":{"value":"test","type":"include"}}'
+      )}`
+    );
+
+    advancedFilter
+      .find('input')
+      .first()
+      .simulate('change', { target: { value: '' } });
+
+    expect(history.length).toBe(3);
+    expect(history.location.search).toBe('?');
+  });
+
+  it('updates filter query params on date filter', () => {
+    const wrapper = createWrapper();
+
+    const advancedFilter = wrapper.find(AdvancedFilter);
+    advancedFilter.find(Link).simulate('click');
+    advancedFilter
+      .find('input')
+      .last()
+      .simulate('change', { target: { value: '2019-08-06' } });
+
+    expect(history.length).toBe(2);
+    expect(history.location.search).toBe(
+      `?filters=${encodeURIComponent('{"endDate":{"endDate":"2019-08-06"}}')}`
+    );
+
+    advancedFilter
+      .find('input')
+      .last()
+      .simulate('change', { target: { value: '' } });
+
+    expect(history.length).toBe(3);
+    expect(history.location.search).toBe('?');
+  });
+
+  it('updates sort query params on sort', () => {
+    const wrapper = createWrapper();
+
+    const button = wrapper.find(ListItemText).first();
+    expect(button.text()).toEqual('investigations.title');
+    button.simulate('click');
+
+    expect(history.length).toBe(2);
+    expect(history.location.search).toBe(
+      `?sort=${encodeURIComponent('{"title":"asc"}')}`
+    );
+  });
+
+  it('renders fine with incomplete data', () => {
+    (useInvestigationCount as jest.Mock).mockReturnValue({});
+    (useInvestigationsPaginated as jest.Mock).mockReturnValue({});
+
+    expect(() => createWrapper()).not.toThrowError();
+  });
 });
