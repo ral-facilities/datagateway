2dd72303d62da9245c6c205805a3496624ce2c70
Joel Davies <joel.davies@stfc.ac.uk>
2021-10-26 15:55:01 +0100

    Add e2e test for new behaviour (#869)

--- packages/datagateway-download/cypress/integration/adminDownloadStatus.spec.ts
+++ packages/datagateway-download/cypress/integration/adminDownloadStatus.spec.ts
@@ -57,6 +57,12 @@ describe('Admin Download Status', () => {
       });
   });
 
+  it('should be able to scroll down and load more rows', () => {
+    cy.get('[aria-rowcount="50"]').should('exist');
+    cy.get('[aria-label="grid"]').scrollTo('bottom');
+    cy.get('[aria-rowcount="75"]').should('exist');
+  });
+
   describe('should be able to sort download items by', () => {
     it('ascending order', () => {
       cy.get('.react-draggable')
