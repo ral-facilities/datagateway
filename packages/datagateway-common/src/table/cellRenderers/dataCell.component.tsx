5b4173d1c05c61bd04ae6b116ce4fbca42c5b935
Louise Davies <louise.davies@stfc.ac.uk>
2021-10-28 09:49:38 +0100

    Improve tooltips by - recalculating tooltips when table columns resize - ensure they display the text corresponding to what's in the cell - use arrow prop to simplify arrowtooltip component

--- packages/datagateway-common/src/table/cellRenderers/dataCell.component.tsx
+++ packages/datagateway-common/src/table/cellRenderers/dataCell.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TableCellProps, TableCellRenderer } from 'react-virtualized';
 import { TableCell, Typography, useMediaQuery } from '@material-ui/core';
-import ArrowTooltip from '../../arrowtooltip.component';
+import ArrowTooltip, { getTooltipText } from '../../arrowtooltip.component';
 
 type CellRendererProps = TableCellProps & {
   className: string;
@@ -13,9 +13,12 @@ const DataCell = React.memo(
     const { className, dataKey, rowData, cellContentRenderer } = props;
 
     // use . in dataKey name to drill down into nested row data
-    const cellValue = dataKey.split('.').reduce(function (prev, curr) {
-      return prev ? prev[curr] : null;
-    }, rowData);
+    // if cellContentRenderer not provided
+    const cellContent = cellContentRenderer
+      ? cellContentRenderer(props)
+      : dataKey.split('.').reduce(function (prev, curr) {
+          return prev ? prev[curr] : null;
+        }, rowData);
 
     const smWindow = !useMediaQuery('(min-width: 960px)');
     return (
@@ -26,9 +29,9 @@ const DataCell = React.memo(
         variant="body"
         style={smWindow ? { paddingLeft: 8, paddingRight: 8 } : {}}
       >
-        <ArrowTooltip title={cellValue} enterDelay={500}>
+        <ArrowTooltip title={getTooltipText(cellContent)} enterDelay={500}>
           <Typography variant="body2" noWrap>
-            {cellContentRenderer ? cellContentRenderer(props) : cellValue}
+            {cellContent}
           </Typography>
         </ArrowTooltip>
       </TableCell>
