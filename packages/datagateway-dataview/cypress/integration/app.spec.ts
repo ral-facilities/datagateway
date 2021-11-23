50c1d8744e3e209348b7e9e0893955f45ea05eb3
Joel Davies <joel.davies@stfc.ac.uk>
2021-11-23 15:51:42 +0000

    Remove accidental duplicate test (#943)

--- packages/datagateway-dataview/cypress/integration/app.spec.ts
+++ packages/datagateway-dataview/cypress/integration/app.spec.ts
@@ -5,34 +5,4 @@ describe('App', () => {
 
     cy.get('#datagateway-dataview').should('be.visible');
   });
-
-  it('should be able to use links on homepage to navigate', () => {
-    cy.visit('/datagateway');
-
-    //Headings
-    cy.contains('1. Explore').click();
-    cy.url().should('include', '/browse/investigation');
-    cy.go('back');
-
-    cy.contains('2. Discover').click();
-    cy.url().should('include', '/search');
-    cy.go('back');
-
-    cy.contains('3. Download').click();
-    cy.url().should('include', '/download');
-    cy.go('back');
-
-    //Images
-    cy.get('[alt="1. Explore"]').click();
-    cy.url().should('include', '/browse/investigation');
-    cy.go('back');
-
-    cy.get('[alt="2. Discover"]').click();
-    cy.url().should('include', '/search');
-    cy.go('back');
-
-    cy.get('[alt="3. Download"]').click();
-    cy.url().should('include', '/download');
-    cy.go('back');
-  });
 });
