775f6e9bb1bcbe5891e1fa89c2f85f81fe7e4352
Louise Davies <louise.davies@stfc.ac.uk>
2021-12-08 15:48:50 +0000

    Fix warning in pageContainer tests

--- packages/datagateway-dataview/src/page/pageContainer.component.test.tsx
+++ packages/datagateway-dataview/src/page/pageContainer.component.test.tsx
@@ -224,7 +224,7 @@ describe('PageContainer - Tests', () => {
     expect(wrapper.exists('[aria-label="filter-message"]')).toBeFalsy();
   });
 
-  it('do not use StyledRouting component on landing pages', () => {
+  it('do not use StyledRouting component on landing pages', async () => {
     (checkInstrumentId as jest.Mock).mockResolvedValueOnce(true);
     (useQueryClient as jest.Mock).mockReturnValue({
       getQueryData: jest.fn(),
@@ -232,6 +232,11 @@ describe('PageContainer - Tests', () => {
     history.replace(`${paths.studyHierarchy.landing.isisStudyLanding}`);
 
     const wrapper = createWrapper();
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
 
     expect(wrapper.exists('StyledRouting')).toBeFalsy();
   });
