3e24d472ac8783a0091e61c0b4caf19257367e1b
Joel Davies <joel.davies@stfc.ac.uk>
2021-12-09 10:48:58 +0000

    Update e2e tests (#954)

--- packages/datagateway-dataview/cypress/integration/homePage.spec.ts
+++ packages/datagateway-dataview/cypress/integration/homePage.spec.ts
@@ -5,29 +5,24 @@ describe('DataGateway HomePage', () => {
 
   it('should be able to use links on homepage to navigate', () => {
     //Headings
-    cy.contains('1. Explore').click();
+    cy.get('[aria-label="Browse data"]').click();
     cy.url().should('include', '/browse/investigation');
     cy.go('back');
 
-    cy.contains('2. Discover').click();
+    cy.get('[aria-label="Search data"]').click();
     cy.url().should('include', '/search');
     cy.go('back');
 
-    cy.contains('3. Download').click();
-    cy.url().should('include', '/download');
-    cy.go('back');
-
-    //Images
-    cy.get('[alt="1. Explore"]').click();
-    cy.url().should('include', '/browse/investigation');
-    cy.go('back');
-
-    cy.get('[alt="2. Discover"]').click();
+    cy.get('[aria-label="Search data"]').click();
     cy.url().should('include', '/search');
     cy.go('back');
 
-    cy.get('[alt="3. Download"]').click();
+    cy.get('[aria-label="Download data"]').click();
     cy.url().should('include', '/download');
+    cy.go('back');
+
+    cy.get('[aria-label="Read more"]').click();
+    cy.url().should('equal', 'https://www.isis.stfc.ac.uk/Pages/About.aspx');
     cy.go('back');
   });
 });
