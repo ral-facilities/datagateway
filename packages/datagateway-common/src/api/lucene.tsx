3471682e4635313e3fc291a2168ee673add0cae3
Joel Davies <joel.davies@stfc.ac.uk>
2021-12-13 14:13:42 +0000

    Remove console.log (#948)

--- packages/datagateway-common/src/api/lucene.tsx
+++ packages/datagateway-common/src/api/lucene.tsx
@@ -75,8 +75,6 @@ export const fetchLuceneData = async (
     maxCount: params.maxCount ? params.maxCount : 300,
   };
 
-  console.log(params.maxCount);
-
   return axios
     .get(`${settings.icatUrl}/lucene/data`, {
       params: queryParams,
