943dc1a87b03f0add43b1e888233235e75e54455
Louise Davies <louise.davies@stfc.ac.uk>
2022-02-08 14:07:15 +0000

    Upgraded react-scripts and applied all changes required in SG i.e. other package updates, SVG changes, flushPromises change & tsconfig changes

--- packages/datagateway-download/src/setupTests.ts
+++ packages/datagateway-download/src/setupTests.ts
@@ -12,7 +12,7 @@ if (typeof window.URL.createObjectURL === 'undefined') {
   Object.defineProperty(window.URL, 'createObjectURL', { value: noOp });
 }
 
-export const flushPromises = (): Promise<void> => new Promise(setImmediate);
+export const flushPromises = (): Promise<void> => new Promise(process.nextTick);
 
 // Mock lodash.debounce to return the function we want to call.
 jest.mock('lodash.debounce', () => (fn: (args: unknown) => unknown) => fn);
