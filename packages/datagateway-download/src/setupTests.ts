981b253c4069199c231e11d20942e55625656e15
Louise Davies <louise.davies@stfc.ac.uk>
2022-02-08 16:41:06 +0000

    Use setTimeout instead of process.nextTick in flushPromises

--- packages/datagateway-download/src/setupTests.ts
+++ packages/datagateway-download/src/setupTests.ts
@@ -12,7 +12,8 @@ if (typeof window.URL.createObjectURL === 'undefined') {
   Object.defineProperty(window.URL, 'createObjectURL', { value: noOp });
 }
 
-export const flushPromises = (): Promise<void> => new Promise(process.nextTick);
+export const flushPromises = (): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve));
 
 // Mock lodash.debounce to return the function we want to call.
 jest.mock('lodash.debounce', () => (fn: (args: unknown) => unknown) => fn);
