425c74e69501d3873769df160b7a0d979ae5bf30
Louise Davies <louise.davies@stfc.ac.uk>
2022-04-01 10:15:44 +0100

    #1198 - fix e2e test for DOI redirect

--- packages/datagateway-dataview/cypress/integration/doiRedirect.spec.ts
+++ packages/datagateway-dataview/cypress/integration/doiRedirect.spec.ts
@@ -1,17 +1,10 @@
 describe('DOI Redirect', () => {
   beforeEach(() => {
     cy.login();
-    cy.intercept('**/investigations/*').as('getInvestigation');
-    cy.intercept('**/instruments/*').as('getInstrument');
-    cy.intercept('**/facilitycycles/*').as('getFacilityCycle');
   });
 
   it('should redirect to correct page if valid id', () => {
-    cy.visit('/doi-redirect/LILS/investigation/1').wait([
-      '@getInvestigation',
-      '@getInstrument',
-      '@getFacilityCycle',
-    ]);
+    cy.visit('/doi-redirect/LILS/investigation/1');
 
     cy.url().should(
       'include',
@@ -20,11 +13,7 @@ describe('DOI Redirect', () => {
   });
 
   it('should redirect to homepage if invalid id', () => {
-    cy.visit('/doi-redirect/LILS/investigation/100000000000').wait([
-      '@getInvestigation',
-      '@getInstrument',
-      '@getFacilityCycle',
-    ]);
+    cy.visit('/doi-redirect/LILS/investigation/100000000000');
 
     cy.url().should('include', '/datagateway');
   });
