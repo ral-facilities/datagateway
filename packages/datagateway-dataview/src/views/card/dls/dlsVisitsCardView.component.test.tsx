3887207e6da4902cc7c36aa9391a65d911596ead
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-08-16 09:28:36 +0100

    Applying overwritten fix to unit tests
    This ensures the correct button is pressed in certain card view unit tests. See the PR for more on this

--- packages/datagateway-dataview/src/views/card/dls/dlsVisitsCardView.component.test.tsx
+++ packages/datagateway-dataview/src/views/card/dls/dlsVisitsCardView.component.test.tsx
@@ -125,7 +125,7 @@ describe('DLS Visits - Card View', () => {
     const wrapper = createWrapper();
 
     const advancedFilter = wrapper.find(AdvancedFilter);
-    advancedFilter.find('button').simulate('click');
+    advancedFilter.find('button').last().simulate('click');
     advancedFilter
       .find('input')
       .first()
