45fc7fdb5fb5b6e36ebcee7c3deb2c10616455f3
Kenneth <kenneth.ng.nym@outlook.com>
2022-09-27 16:48:54 +0100

    Migrate selectHeader test to RTL

--- packages/datagateway-common/src/table/headerRenderers/selectHeader.component.test.tsx
+++ packages/datagateway-common/src/table/headerRenderers/selectHeader.component.test.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import type { UserEvent } from '@testing-library/user-event/setup/setup';
+import * as React from 'react';
 import SelectHeader from './selectHeader.component';
 
 describe('Select column header component', () => {
+  let user: UserEvent;
   const setLastChecked = jest.fn();
   const onCheck = jest.fn();
   const onUncheck = jest.fn();
@@ -16,64 +19,62 @@ describe('Select column header component', () => {
     loading: false,
   };
 
+  beforeEach(() => {
+    user = userEvent.setup();
+  });
+
   afterEach(() => {
     setLastChecked.mockClear();
     onCheck.mockClear();
     onUncheck.mockClear();
   });
 
-  it('renders correctly when unchecked', () => {
-    const wrapper = shallow(<SelectHeader {...selectHeaderProps} />);
-    expect(wrapper).toMatchSnapshot();
+  it('renders correctly when unchecked', async () => {
+    const { asFragment } = render(<SelectHeader {...selectHeaderProps} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when indeterminate', () => {
-    const wrapper = shallow(
+  it('renders correctly when indeterminate', async () => {
+    const { asFragment } = render(
       <SelectHeader {...selectHeaderProps} selectedRows={[1]} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when checked', () => {
-    const wrapper = shallow(
+  it('renders correctly when checked', async () => {
+    const { asFragment } = render(
       <SelectHeader {...selectHeaderProps} selectedRows={[1, 2, 3]} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when selectedRows is undefined', () => {
-    const wrapper = shallow(
+  it('renders correctly when selectedRows is undefined', async () => {
+    const { asFragment } = render(
       <SelectHeader {...selectHeaderProps} selectedRows={undefined} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when loading is true', () => {
-    const wrapper = shallow(
+  it('renders correctly when loading is true', async () => {
+    const { asFragment } = render(
       <SelectHeader
         {...selectHeaderProps}
         loading={true}
         selectedRows={undefined}
       />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('calls onCheck when not all rows are selected and the checkbox is clicked', () => {
-    const wrapper = shallow(
-      <SelectHeader {...selectHeaderProps} selectedRows={[1]} />
-    );
-
-    wrapper.childAt(0).childAt(0).childAt(0).prop('onClick')();
+  it('calls onCheck when not all rows are selected and the checkbox is clicked', async () => {
+    render(<SelectHeader {...selectHeaderProps} selectedRows={[1]} />);
+    await user.click(await screen.findByRole('checkbox'));
     expect(onCheck).toHaveBeenCalledWith([1, 2, 3]);
   });
 
-  it('calls onUncheck when all rows are selected and the checkbox is clicked', () => {
-    const wrapper = shallow(
-      <SelectHeader {...selectHeaderProps} selectedRows={[1, 2, 3]} />
-    );
-
-    wrapper.childAt(0).childAt(0).childAt(0).prop('onClick')();
+  it('calls onUncheck when all rows are selected and the checkbox is clicked', async () => {
+    render(<SelectHeader {...selectHeaderProps} selectedRows={[1, 2, 3]} />);
+    await user.click(await screen.findByRole('checkbox'));
     expect(onUncheck).toHaveBeenCalledWith([1, 2, 3]);
   });
 });
