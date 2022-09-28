f82a81a51a5767f6329a8a15b3608a5cba7dfd95
Kenneth <kenneth.ng.nym@outlook.com>
2022-09-28 14:25:47 +0100

    Migrate selectCell test to RTL

--- packages/datagateway-common/src/table/cellRenderers/selectCell.component.test.tsx
+++ packages/datagateway-common/src/table/cellRenderers/selectCell.component.test.tsx
@@ -1,8 +1,11 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import type { UserEvent } from '@testing-library/user-event/setup/setup';
 import React from 'react';
-import { shallow } from 'enzyme';
 import SelectCell from './selectCell.component';
 
 describe('Select cell component', () => {
+  let user: UserEvent;
   const setLastChecked = jest.fn();
   const onCheck = jest.fn();
   const onUncheck = jest.fn();
@@ -36,93 +39,92 @@ describe('Select cell component', () => {
     loading: false,
   };
 
+  beforeEach(() => {
+    user = userEvent.setup();
+  });
+
   afterEach(() => {
     setLastChecked.mockClear();
     onCheck.mockClear();
     onUncheck.mockClear();
   });
 
-  it('renders correctly when unchecked', () => {
-    const wrapper = shallow(<SelectCell {...selectCellProps} />);
-    expect(wrapper).toMatchSnapshot();
+  it('renders correctly when unchecked', async () => {
+    const { asFragment } = render(<SelectCell {...selectCellProps} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when checked', () => {
-    const wrapper = shallow(
+  it('renders correctly when checked', async () => {
+    const { asFragment } = render(
       <SelectCell {...selectCellProps} selectedRows={[3]} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when selectedRows is undefined', () => {
-    const wrapper = shallow(
+  it('renders correctly when selectedRows is undefined', async () => {
+    const { asFragment } = render(
       <SelectCell {...selectCellProps} selectedRows={undefined} />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('renders correctly when selectedRows loading is true', () => {
-    const wrapper = shallow(
+  it('renders correctly when selectedRows loading is true', async () => {
+    const { asFragment } = render(
       <SelectCell
         {...selectCellProps}
         loading={true}
         selectedRows={undefined}
       />
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('calls setLastChecked when checkbox is clicked', () => {
-    const wrapper = shallow(<SelectCell {...selectCellProps} />);
-
-    wrapper.find('.tour-dataview-add-to-cart').prop('onClick')(
-      new MouseEvent('click')
+  it('calls setLastChecked when checkbox is clicked', async () => {
+    render(<SelectCell {...selectCellProps} />);
+    await user.click(
+      await screen.findByRole('checkbox', { name: 'select row 2' })
     );
     expect(setLastChecked).toHaveBeenCalledWith(2);
   });
 
-  it('calls onCheck when the row is unselected and the checkbox is clicked', () => {
-    const wrapper = shallow(<SelectCell {...selectCellProps} />);
-
-    wrapper.find('.tour-dataview-add-to-cart').prop('onClick')(
-      new MouseEvent('click')
+  it('calls onCheck when the row is unselected and the checkbox is clicked', async () => {
+    render(<SelectCell {...selectCellProps} />);
+    await user.click(
+      await screen.findByRole('checkbox', { name: 'select row 2' })
     );
     expect(onCheck).toHaveBeenCalledWith([3]);
   });
 
-  it('calls onUncheck when the row is selected and the checkbox is clicked', () => {
-    const wrapper = shallow(
-      <SelectCell {...selectCellProps} selectedRows={[3]} />
-    );
-
-    wrapper.find('.tour-dataview-add-to-cart').prop('onClick')(
-      new MouseEvent('click')
+  it('calls onUncheck when the row is selected and the checkbox is clicked', async () => {
+    render(<SelectCell {...selectCellProps} selectedRows={[3]} />);
+    await user.click(
+      await screen.findByRole('checkbox', { name: 'select row 2' })
     );
     expect(onUncheck).toHaveBeenCalledWith([3]);
   });
-
-  it('calls onCheck when the row is selected via shift-click and the checkbox is clicked', () => {
-    const wrapper = shallow(
-      <SelectCell {...selectCellProps} lastChecked={0} />
-    );
-
-    wrapper.find('.tour-dataview-add-to-cart').prop('onClick')(
-      new MouseEvent('click', { shiftKey: true })
+  it('calls onCheck when the row is selected via shift-click and the checkbox is clicked', async () => {
+    render(<SelectCell {...selectCellProps} lastChecked={0} />);
+    fireEvent.click(
+      await screen.findByRole('checkbox', {
+        name: 'select row 2',
+      }),
+      { shiftKey: true }
     );
     expect(onCheck).toHaveBeenCalledWith([1, 2, 3]);
   });
-
-  it('calls onUncheck when the row is unselected via shift-click and the checkbox is clicked', () => {
-    const wrapper = shallow(
+  it('calls onUncheck when the row is unselected via shift-click and the checkbox is clicked', async () => {
+    render(
       <SelectCell
         {...selectCellProps}
         lastChecked={0}
         selectedRows={[1, 2, 3]}
       />
     );
-
-    wrapper.find('.tour-dataview-add-to-cart').prop('onClick')(
-      new MouseEvent('click', { shiftKey: true })
+    fireEvent.click(
+      await screen.findByRole('checkbox', {
+        name: 'select row 2',
+      }),
+      { shiftKey: true }
     );
     expect(onUncheck).toHaveBeenCalledWith([1, 2, 3]);
   });
