58dcbca6f321dbc8eac14fcdca05f21b831def9d
Kenneth <kenneth.ng.nym@outlook.com>
2022-10-19 11:07:54 +0100

    Update isisMyDataTable tests

--- packages/datagateway-common/src/index.tsx
+++ packages/datagateway-common/src/index.tsx
@@ -79,9 +79,6 @@ export { default as InvestigationDetailsPanel } from './detailsPanels/investigat
 export { default as DatasetDetailsPanel } from './detailsPanels/datasetDetailsPanel.component';
 export { default as DatafileDetailsPanel } from './detailsPanels/datasetDetailsPanel.component';
 
-export { default as buildInvestigationUrl } from './urlBuilders/buildInvestigationUrl';
-export { default as buildDatasetUrl } from './urlBuilders/buildDatasetUrl';
-export { default as buildDatafileUrl } from './urlBuilders/buildDatafileUrl';
 export * from './urlBuilders';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
