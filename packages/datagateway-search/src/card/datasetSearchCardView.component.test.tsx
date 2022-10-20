c3c64308f0b45fb03546615abf9620e4423cf15f
Kenneth <kenneth.ng.nym@outlook.com>
2022-10-20 15:52:40 +0100

    Migrate investigationSearchCardView test to RTL Also removed screen.debug call in datasetSearchCardView test

--- packages/datagateway-search/src/card/datasetSearchCardView.component.test.tsx
+++ packages/datagateway-search/src/card/datasetSearchCardView.component.test.tsx
@@ -210,8 +210,6 @@ describe('Dataset - Card View', () => {
   it('updates sort query params on sort', async () => {
     renderComponent();
 
-    screen.debug(undefined, 10000000);
-
     await user.click(
       await screen.findByRole('button', { name: 'Sort by DATASETS.NAME' })
     );
--- packages/datagateway-search/src/card/investigationSearchCardView.component.test.tsx
+++ packages/datagateway-search/src/card/investigationSearchCardView.component.test.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import * as React from 'react';
 import {
   dGCommonInitialState,
-  useAllFacilityCycles,
-  useLuceneSearch,
-  useInvestigationCount,
-  useInvestigationsPaginated,
-  useInvestigationsDatasetCount,
-  useInvestigationSizes,
   Investigation,
   StateType,
-  AdvancedFilter,
-  CardView,
-  InvestigationDetailsPanel,
-  ISISInvestigationDetailsPanel,
-  DLSVisitDetailsPanel,
+  useAllFacilityCycles,
+  useInvestigationCount,
+  useInvestigationsDatasetCount,
+  useInvestigationSizes,
+  useInvestigationsPaginated,
+  useLuceneSearch,
 } from 'datagateway-common';
 import InvestigationSearchCardView from './investigationSearchCardView.component';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import { mount, ReactWrapper } from 'enzyme';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
 import { createMemoryHistory, History } from 'history';
 import { initialState as dgSearchInitialState } from '../state/reducers/dgsearch.reducer';
-import { ListItemText } from '@mui/material';
 
 import {
   applyDatePickerWorkaround,
   cleanupDatePickerWorkaround,
 } from '../setupTests';
+import {
+  render,
+  RenderResult,
+  screen,
+  waitFor,
+  within,
+} from '@testing-library/react';
+import { UserEvent } from '@testing-library/user-event/setup/setup';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('datagateway-common', () => {
   const originalModule = jest.requireActual('datagateway-common');
@@ -52,9 +54,10 @@ describe('Investigation - Card View', () => {
   let state: StateType;
   let cardData: Investigation[];
   let history: History;
+  let user: UserEvent;
 
-  const createWrapper = (hierarchy?: string): ReactWrapper => {
-    return mount(
+  const renderComponent = (hierarchy?: string): RenderResult =>
+    render(
       <Provider store={mockStore(state)}>
         <Router history={history}>
           <QueryClientProvider client={new QueryClient()}>
@@ -63,7 +66,6 @@ describe('Investigation - Card View', () => {
         </Router>
       </Provider>
     );
-  };
 
   beforeEach(() => {
     cardData = [
@@ -108,8 +110,9 @@ describe('Investigation - Card View', () => {
       },
     ];
     history = createMemoryHistory();
-
     mockStore = configureStore([thunk]);
+    user = userEvent.setup();
+
     state = JSON.parse(
       JSON.stringify({
         dgcommon: dGCommonInitialState,
@@ -169,60 +172,19 @@ describe('Investigation - Card View', () => {
 
   //The below tests are modified from datasetSearchCardView
 
-  it('renders correctly', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.find('CardView').props()).toMatchSnapshot();
-  });
+  it('updates filter query params on text filter', async () => {
+    renderComponent();
 
-  it('calls the correct data fetching hooks on load', () => {
-    (useLuceneSearch as jest.Mock).mockReturnValue({
-      data: [1],
+    await user.click(
+      await screen.findByRole('button', { name: 'advanced_filters.show' })
+    );
+
+    const filterInput = await screen.findByRole('textbox', {
+      name: 'Filter by investigations.title',
+      hidden: true,
     });
 
-    createWrapper();
-
-    expect(useLuceneSearch).toHaveBeenCalledWith('Investigation', {
-      searchText: '',
-      startDate: null,
-      endDate: null,
-      maxCount: 300,
-    });
-
-    expect(useInvestigationCount).toHaveBeenCalledWith([
-      {
-        filterType: 'where',
-        filterValue: JSON.stringify({
-          id: { in: [1] },
-        }),
-      },
-    ]);
-    expect(useInvestigationsPaginated).toHaveBeenCalledWith([
-      {
-        filterType: 'where',
-        filterValue: JSON.stringify({
-          id: { in: [1] },
-        }),
-      },
-      {
-        filterType: 'include',
-        filterValue: JSON.stringify({
-          investigationInstruments: 'instrument',
-        }),
-      },
-    ]);
-    expect(useInvestigationsDatasetCount).toHaveBeenCalledWith(cardData);
-    expect(useInvestigationSizes).toHaveBeenCalledWith(undefined);
-  });
-
-  it('updates filter query params on text filter', () => {
-    const wrapper = createWrapper();
-
-    const advancedFilter = wrapper.find(AdvancedFilter);
-    advancedFilter.find('button').first().simulate('click');
-    advancedFilter
-      .find('input')
-      .first()
-      .simulate('change', { target: { value: 'test' } });
+    await user.type(filterInput, 'test');
 
     expect(history.location.search).toBe(
       `?filters=${encodeURIComponent(
@@ -230,46 +192,49 @@ describe('Investigation - Card View', () => {
       )}`
     );
 
-    advancedFilter
-      .find('input')
-      .first()
-      .simulate('change', { target: { value: '' } });
-
+    await user.clear(filterInput);
     expect(history.location.search).toBe('?');
   });
 
-  it('updates filter query params on date filter', () => {
+  it('updates filter query params on date filter', async () => {
     applyDatePickerWorkaround();
 
-    const wrapper = createWrapper();
+    renderComponent();
 
-    const advancedFilter = wrapper.find(AdvancedFilter);
-    advancedFilter.find('button').first().simulate('click');
-    advancedFilter
-      .find('input')
-      .last()
-      .simulate('change', { target: { value: '2019-08-06' } });
+    await user.click(
+      await screen.findByRole('button', { name: 'advanced_filters.show' })
+    );
+
+    await user.type(
+      await screen.findByRole('textbox', {
+        name: 'investigations.details.end_date filter to',
+      }),
+      '2019-08-06'
+    );
 
     expect(history.location.search).toBe(
       `?filters=${encodeURIComponent('{"endDate":{"endDate":"2019-08-06"}}')}`
     );
 
-    advancedFilter
-      .find('input')
-      .last()
-      .simulate('change', { target: { value: '' } });
+    await user.clear(
+      await screen.findByRole('textbox', {
+        name: 'investigations.details.end_date filter to',
+      })
+    );
 
     expect(history.location.search).toBe('?');
 
     cleanupDatePickerWorkaround();
   });
 
-  it('updates sort query params on sort', () => {
-    const wrapper = createWrapper();
+  it('updates sort query params on sort', async () => {
+    renderComponent();
 
-    const button = wrapper.find(ListItemText).first();
-    expect(button.text()).toEqual('investigations.title');
-    button.find('div').simulate('click');
+    await user.click(
+      await screen.findByRole('button', {
+        name: 'Sort by INVESTIGATIONS.TITLE',
+      })
+    );
 
     expect(history.location.search).toBe(
       `?sort=${encodeURIComponent('{"title":"asc"}')}`
@@ -280,42 +245,48 @@ describe('Investigation - Card View', () => {
     (useInvestigationCount as jest.Mock).mockReturnValue({});
     (useInvestigationsPaginated as jest.Mock).mockReturnValue({});
 
-    expect(() => createWrapper()).not.toThrowError();
+    renderComponent();
+
+    expect(screen.queryAllByTestId('card')).toHaveLength(0);
   });
 
-  it('renders generic link & pending count correctly', () => {
+  it('renders generic link & pending count correctly', async () => {
     (useInvestigationsDatasetCount as jest.Mock).mockImplementation(() => [
       {
         isFetching: true,
       },
     ]);
-    const wrapper = createWrapper();
+    renderComponent();
 
-    expect(wrapper.find(CardView).find('a').first().prop('href')).toEqual(
-      `/browse/investigation/1/dataset`
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(within(card).getByRole('link', { name: 'Test 1' })).toHaveAttribute(
+      'href',
+      '/browse/investigation/1/dataset'
     );
-    expect(wrapper.find(CardView).find('a').first().text()).toEqual('Test 1');
     expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[data-testid="card-info-data-investigations.dataset_count"]')
-        .text()
-    ).toEqual('Calculating...');
+      within(card).getByTestId('card-info-data-investigations.dataset_count')
+    ).toHaveTextContent('Calculating...');
   });
 
-  it("renders DLS link correctly and doesn't allow for cart selection or download", () => {
-    const wrapper = createWrapper('dls');
+  it("renders DLS link correctly and doesn't allow for cart selection or download", async () => {
+    renderComponent('dls');
 
-    expect(wrapper.find(CardView).find('a').first().prop('href')).toEqual(
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(within(card).getByRole('link', { name: 'Test 1' })).toHaveAttribute(
+      'href',
       '/browse/proposal/Investigation test name/investigation/1/dataset'
     );
-    expect(wrapper.find(CardView).find('a').first().text()).toEqual('Test 1');
-    expect(wrapper.exists('#add-to-cart-btn-1')).toBe(false);
-    expect(wrapper.exists('#download-btn-1')).toBe(false);
+    expect(
+      screen.queryByRole('button', { name: 'buttons.add_to_cart' })
+    ).toBeNull();
+    expect(
+      screen.queryByRole('button', { name: 'buttons.download' })
+    ).toBeNull();
   });
 
-  it('renders ISIS link & file sizes correctly', () => {
+  it('renders ISIS link & file sizes correctly', async () => {
     (useAllFacilityCycles as jest.Mock).mockReturnValue({
       data: [
         {
@@ -327,42 +298,33 @@ describe('Investigation - Card View', () => {
       ],
     });
 
-    const wrapper = createWrapper('isis');
+    renderComponent('isis');
 
     expect(useInvestigationSizes).toHaveBeenCalledWith(cardData);
     expect(useInvestigationsDatasetCount).toHaveBeenCalledWith(undefined);
 
-    expect(wrapper.find(CardView).find('a').first().prop('href')).toEqual(
-      `/browse/instrument/4/facilityCycle/6/investigation/1/dataset`
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(
+      within(card).getByRole('link', {
+        name: 'Test 1',
+      })
+    ).toHaveAttribute(
+      'href',
+      '/browse/instrument/4/facilityCycle/6/investigation/1/dataset'
     );
-    expect(wrapper.find(CardView).find('a').first().text()).toEqual('Test 1');
-    expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[data-testid="card-info-data-investigations.size"]')
-        .text()
-    ).toEqual('1 B');
   });
 
-  it('displays DOI and renders the expected Link ', () => {
-    const wrapper = createWrapper();
-    expect(
-      wrapper
-        .find('[data-testid="investigation-search-card-doi-link"]')
-        .first()
-        .text()
-    ).toEqual('doi 1');
+  it('displays DOI and renders the expected Link ', async () => {
+    renderComponent();
 
-    expect(
-      wrapper
-        .find('[data-testid="investigation-search-card-doi-link"]')
-        .first()
-        .prop('href')
-    ).toEqual('https://doi.org/doi 1');
+    expect(await screen.findByRole('link', { name: 'doi 1' })).toHaveAttribute(
+      'href',
+      'https://doi.org/doi 1'
+    );
   });
 
-  it('does not render ISIS link when instrumentId cannot be found', () => {
+  it('does not render ISIS link when instrumentId cannot be found', async () => {
     (useAllFacilityCycles as jest.Mock).mockReturnValue({
       data: [
         {
@@ -379,60 +341,53 @@ describe('Investigation - Card View', () => {
       data: cardData,
       fetchNextPage: jest.fn(),
     });
-    const wrapper = createWrapper('isis');
+    renderComponent('isis');
 
-    expect(wrapper.find(CardView).first().find('a')).toHaveLength(1);
-    expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[aria-label="card-title"]')
-        .last()
-        .text()
-    ).toEqual('Test 1');
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    await waitFor(() => {
+      expect(within(card).queryByRole('link', { name: 'Test 1' })).toBeNull();
+    });
+    expect(within(card).getByLabelText('card-title')).toHaveTextContent(
+      'Test 1'
+    );
   });
 
-  it('displays only the dataset name when there is no generic investigation to link to', () => {
+  it('displays only the dataset name when there is no generic investigation to link to', async () => {
     delete cardData[0].investigation;
     (useInvestigationsPaginated as jest.Mock).mockReturnValue({
       data: cardData,
       fetchNextPage: jest.fn(),
     });
 
-    const wrapper = createWrapper('data');
+    renderComponent('data');
 
-    expect(wrapper.find(CardView).first().find('a')).toHaveLength(2);
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(within(card).getAllByRole('link')).toHaveLength(2);
     expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[aria-label="card-title"]')
-        .last()
-        .text()
-    ).toEqual('Test 1');
+      within(card).getByRole('link', { name: 'Test 1' })
+    ).toBeInTheDocument();
   });
 
-  it('displays only the dataset name when there is no DLS investigation to link to', () => {
+  it('displays only the dataset name when there is no DLS investigation to link to', async () => {
     delete cardData[0].investigation;
     (useInvestigationsPaginated as jest.Mock).mockReturnValue({
       data: cardData,
       fetchNextPage: jest.fn(),
     });
 
-    const wrapper = createWrapper('dls');
+    renderComponent('dls');
 
-    expect(wrapper.find(CardView).first().find('a')).toHaveLength(2);
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(within(card).getAllByRole('link')).toHaveLength(2);
     expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[aria-label="card-title"]')
-        .last()
-        .text()
-    ).toEqual('Test 1');
+      within(card).getByRole('link', { name: 'Test 1' })
+    ).toBeInTheDocument();
   });
 
-  it('displays only the dataset name when there is no ISIS investigation to link to', () => {
+  it('displays only the dataset name when there is no ISIS investigation to link to', async () => {
     (useAllFacilityCycles as jest.Mock).mockReturnValue({
       data: [
         {
@@ -449,42 +404,37 @@ describe('Investigation - Card View', () => {
       fetchNextPage: jest.fn(),
     });
 
-    const wrapper = createWrapper('isis');
+    renderComponent('isis');
 
-    expect(wrapper.find(CardView).first().find('a')).toHaveLength(2);
+    const card = (await screen.findAllByTestId('card'))[0];
+
+    expect(within(card).getAllByRole('link')).toHaveLength(2);
     expect(
-      wrapper
-        .find(CardView)
-        .first()
-        .find('[aria-label="card-title"]')
-        .last()
-        .text()
-    ).toEqual('Test 1');
+      within(card).getByRole('link', { name: 'Test 1' })
+    ).toBeInTheDocument();
   });
 
-  it('displays generic details panel when expanded', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.find(InvestigationDetailsPanel).exists()).toBeFalsy();
-    wrapper
-      .find('[aria-label="card-more-info-expand"]')
-      .last()
-      .simulate('click');
-
-    expect(wrapper.find(InvestigationDetailsPanel).exists()).toBeTruthy();
+  it('displays generic details panel when expanded', async () => {
+    renderComponent();
+    await user.click(
+      await screen.findByRole('button', { name: 'card-more-info-expand' })
+    );
+    expect(
+      await screen.findByTestId('investigation-details-panel')
+    ).toBeTruthy();
   });
 
-  it('displays correct details panel for ISIS when expanded', () => {
-    const wrapper = createWrapper('isis');
-    expect(wrapper.find(ISISInvestigationDetailsPanel).exists()).toBeFalsy();
-    wrapper
-      .find('[aria-label="card-more-info-expand"]')
-      .last()
-      .simulate('click');
-
-    expect(wrapper.find(ISISInvestigationDetailsPanel).exists()).toBeTruthy();
+  it('displays correct details panel for ISIS when expanded', async () => {
+    renderComponent('isis');
+    await user.click(
+      await screen.findByRole('button', { name: 'card-more-info-expand' })
+    );
+    expect(
+      await screen.findByTestId('investigation-details-panel')
+    ).toBeTruthy();
   });
 
-  it('can navigate using the details panel for ISIS when there are facility cycles', () => {
+  it('can navigate using the details panel for ISIS when there are facility cycles', async () => {
     (useAllFacilityCycles as jest.Mock).mockReturnValue({
       data: [
         {
@@ -496,29 +446,30 @@ describe('Investigation - Card View', () => {
       ],
     });
 
-    const wrapper = createWrapper('isis');
-    expect(wrapper.find(ISISInvestigationDetailsPanel).exists()).toBeFalsy();
-    wrapper
-      .find('[aria-label="card-more-info-expand"]')
-      .last()
-      .simulate('click');
+    renderComponent('isis');
+    await user.click(
+      await screen.findByRole('button', { name: 'card-more-info-expand' })
+    );
+    expect(
+      await screen.findByTestId('investigation-details-panel')
+    ).toBeTruthy();
 
-    expect(wrapper.find(ISISInvestigationDetailsPanel).exists()).toBeTruthy();
+    await user.click(
+      await screen.findByRole('tab', {
+        name: 'investigations.details.datasets',
+      })
+    );
 
-    wrapper.find('#investigation-datasets-tab').last().simulate('click');
     expect(history.location.pathname).toBe(
       '/browse/instrument/4/facilityCycle/4/investigation/1/dataset'
     );
   });
 
-  it('displays correct details panel for DLS when expanded', () => {
-    const wrapper = createWrapper('dls');
-    expect(wrapper.find(DLSVisitDetailsPanel).exists()).toBeFalsy();
-    wrapper
-      .find('[aria-label="card-more-info-expand"]')
-      .last()
-      .simulate('click');
-
-    expect(wrapper.find(DLSVisitDetailsPanel).exists()).toBeTruthy();
+  it('displays correct details panel for DLS when expanded', async () => {
+    renderComponent('dls');
+    await user.click(
+      await screen.findByRole('button', { name: 'card-more-info-expand' })
+    );
+    expect(await screen.findByTestId('visit-details-panel')).toBeTruthy();
   });
 });
