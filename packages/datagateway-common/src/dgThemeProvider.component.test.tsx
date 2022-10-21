f208edab793c030cedcba7713da8f0856175b50e
Kenneth <kenneth.ng.nym@outlook.com>
2022-10-21 11:30:03 +0100

    Improve dgThemeProvider tests

--- packages/datagateway-common/src/dgThemeProvider.component.test.tsx
+++ packages/datagateway-common/src/dgThemeProvider.component.test.tsx
@@ -1,42 +1,106 @@
 import React from 'react';
 import { createTheme } from '@mui/material/styles';
 import DGThemeProvider from './dgThemeProvider.component';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import { Box } from '@mui/material';
 import { MicroFrontendId } from './app.types';
 import { SendThemeOptionsType } from './state/actions/actions.types';
 
 describe('DGThemeProvider', () => {
-  it('receives and uses the theme options', () => {
-    // Create a basic theme.
-    const theme = createTheme({
-      palette: {
-        mode: 'dark',
-      },
-    });
-
-    const wrapper = shallow(
+  it('uses default theme before receiving theme event', () => {
+    render(
       <DGThemeProvider>
-        <div>Test</div>
+        <Box data-testid="box" sx={{ bgcolor: 'primary.main' }}>
+          test
+        </Box>
       </DGThemeProvider>
     );
 
-    // Dispatch the theme options event.
+    // value taken from https://mui.com/material-ui/customization/default-theme/
+    // (default value is #1976d2)
+    // mui converts the hex value to CSS rgb expression
+
+    expect(screen.getByTestId('box')).toHaveStyle(
+      'background-color: rgb(25, 118, 210);'
+    );
+  });
+
+  it('uses the theme sent through the theme event', () => {
     document.dispatchEvent(
       new CustomEvent(MicroFrontendId, {
         detail: {
           type: SendThemeOptionsType,
           payload: {
-            theme,
+            theme: createTheme({
+              palette: {
+                primary: {
+                  main: '#123456',
+                },
+              },
+            }),
           },
         },
       })
     );
 
-    // Force update the wrapper as the state will not
-    // update since the theme options are from a global variable.
-    wrapper.instance().forceUpdate();
+    render(
+      <DGThemeProvider>
+        <Box data-testid="box" sx={{ bgcolor: 'primary.main' }}>
+          test
+        </Box>
+      </DGThemeProvider>
+    );
 
-    expect(wrapper.childAt(0).props()).toHaveProperty('theme');
-    expect(wrapper.childAt(0).prop('theme')).toEqual(theme);
+    expect(screen.getByTestId('box')).toHaveStyle(
+      'background-color: rgb(18, 52, 86);'
+    );
+  });
+
+  it('ignores non-theme events', () => {
+    document.dispatchEvent(
+      new CustomEvent(MicroFrontendId, {
+        detail: {
+          type: SendThemeOptionsType,
+          payload: {
+            theme: createTheme({
+              palette: {
+                primary: {
+                  main: '#abcdef',
+                },
+              },
+            }),
+          },
+        },
+      })
+    );
+
+    document.dispatchEvent(
+      new CustomEvent(MicroFrontendId, {
+        detail: {
+          type: 'ImposterEvent',
+          payload: {
+            theme: createTheme(),
+            yellow: 'sus',
+          },
+        },
+      })
+    );
+
+    render(
+      <DGThemeProvider>
+        <Box data-testid="box" sx={{ bgcolor: 'primary.main' }}>
+          test
+        </Box>
+      </DGThemeProvider>
+    );
+
+    // value taken from https://mui.com/material-ui/customization/default-theme/
+    // (default value is #1976d2)
+    // mui converts the hex value to CSS rgb expression
+
+    // expect the dispatched theme to be used.
+    expect(screen.getByTestId('box')).toHaveStyle(
+      'background-color: rgb(171, 205, 239);'
+    );
   });
 });
