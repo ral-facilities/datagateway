785a7c241177b46e1cca4dbbaffa09c361348eb6
Kenneth <kenneth.ng.nym@outlook.com>
2023-02-02 11:28:29 +0000

    Update search help dialog style & rewrite tests

--- packages/datagateway-search/src/search/advancedHelpDialogue.component.test.tsx
+++ packages/datagateway-search/src/search/advancedHelpDialogue.component.test.tsx
@@ -4,13 +4,17 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { dGCommonInitialState } from 'datagateway-common';
 import configureStore from 'redux-mock-store';
-import { mount, ReactWrapper } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import type { RenderResult } from '@testing-library/react';
-import { render } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 
 import { initialState as dgSearchInitialState } from '../state/reducers/dgsearch.reducer';
 import { StateType } from '../state/app.types';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
@@ -27,12 +31,10 @@ function renderComponent({ initialState }): RenderResult {
   );
 }
 
-describe('Advanced help dialogue component tests', () => {
-  let mockStore;
+describe('Advanced help dialogue', () => {
   let state: StateType;
 
   beforeEach(() => {
-    mockStore = configureStore([thunk]);
     state = JSON.parse(
       JSON.stringify({
         dgcommon: dGCommonInitialState,
@@ -41,42 +43,39 @@ describe('Advanced help dialogue component tests', () => {
     );
   });
 
-  const createWrapper = (): ReactWrapper => {
-    return mount(
-      <Provider store={mockStore(state)}>
-        <MemoryRouter>
-          <AdvancedHelpDialogue />
-        </MemoryRouter>
-      </Provider>
-    );
-  };
-
-  it('renders correctly', () => {
-    const { asFragment } = renderComponent({ initialState: state });
-    expect(asFragment()).toMatchSnapshot();
+  it('is hidden initially', () => {
+    renderComponent({ initialState: state });
+    expect(
+      screen.queryByRole('dialog', { name: 'Advanced Search Tips' })
+    ).toBeNull();
   });
 
-  it('can open and close help dialogue', () => {
-    const wrapper = createWrapper();
-    wrapper
-      .find('[aria-label="advanced_search_help.search_options_arialabel"]')
-      .last()
-      .simulate('click');
+  it('opens when search options link is clicked and closes when the close button is clicked', async () => {
+    const user = userEvent.setup();
+
+    renderComponent({ initialState: state });
+
+    await user.click(
+      screen.getByRole('button', {
+        name: 'advanced_search_help.search_options_arialabel',
+      })
+    );
+
     expect(
-      wrapper
-        .find('[aria-labelledby="advanced-search-dialog-title"]')
-        .first()
-        .prop('open')
-    ).toBe(true);
-    wrapper
-      .find('[aria-label="advanced_search_help.close_button_arialabel"]')
-      .last()
-      .simulate('click');
+      screen.getByRole('dialog', { name: 'Advanced Search Tips' })
+    ).toBeInTheDocument();
+
+    await user.click(
+      screen.getByRole('button', {
+        name: 'advanced_search_help.close_button_arialabel',
+      })
+    );
+
+    await waitForElementToBeRemoved(
+      screen.getByRole('dialog', { name: 'Advanced Search Tips' })
+    );
     expect(
-      wrapper
-        .find('[aria-labelledby="advanced-search-dialog-title"]')
-        .first()
-        .prop('open')
-    ).toBe(false);
+      screen.queryByRole('dialog', { name: 'Advanced Search Tips' })
+    ).toBeNull();
   });
 });
