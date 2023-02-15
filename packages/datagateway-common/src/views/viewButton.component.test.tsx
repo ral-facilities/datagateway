6b27ed50f394b825f084c0ca2de26a354d289c7a
Kenneth <kenneth.ng.nym@outlook.com>
2023-02-15 15:23:11 +0000

    Improve viewButton tests

--- packages/datagateway-common/src/views/viewButton.component.test.tsx
+++ packages/datagateway-common/src/views/viewButton.component.test.tsx
@@ -1,27 +1,25 @@
-import { render, screen, type RenderResult } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import type { UserEvent } from '@testing-library/user-event/setup/setup';
 import * as React from 'react';
 import configureStore from 'redux-mock-store';
 import { initialState as dGCommonInitialState } from '../state/reducers/dgcommon.reducer';
-import { StateType } from '../state/app.types';
+import type { StateType } from '../state/app.types';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { MemoryRouter } from 'react-router-dom';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import ViewButton, { ViewProps } from './viewButton.component';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import ViewButton from './viewButton.component';
 
 describe('Generic view button', () => {
-  const mockStore = configureStore([thunk]);
-  const handleButtonChange = jest.fn();
-  let user: UserEvent;
   let state: StateType;
-  let props: ViewProps;
 
-  function renderComponent(props: ViewProps): RenderResult {
-    const store = mockStore(state);
-    return render(
-      <Provider store={store}>
+  function Wrapper({
+    children,
+  }: {
+    children: React.ReactElement;
+  }): JSX.Element {
+    return (
+      <Provider store={configureStore([thunk])(state)}>
         <MemoryRouter
           initialEntries={[
             {
@@ -31,7 +29,7 @@ describe('Generic view button', () => {
           ]}
         >
           <QueryClientProvider client={new QueryClient()}>
-            <ViewButton {...props} />
+            {children}
           </QueryClientProvider>
         </MemoryRouter>
       </Provider>
@@ -39,12 +37,6 @@ describe('Generic view button', () => {
   }
 
   beforeEach(() => {
-    user = userEvent.setup();
-    props = {
-      viewCards: true,
-      handleButtonChange: handleButtonChange,
-      disabled: false,
-    };
     state = JSON.parse(
       JSON.stringify({
         dgdataview: {},
@@ -60,18 +52,40 @@ describe('Generic view button', () => {
     );
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-    handleButtonChange.mockClear();
+  it('displays as view table when card view is enabled', async () => {
+    render(<ViewButton viewCards handleButtonChange={jest.fn()} />, {
+      wrapper: Wrapper,
+    });
+
+    const button = screen.getByRole('button', {
+      name: 'page view app.view_table',
+    });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('app.view_table');
+    expect(screen.getByTestId('ViewListIcon')).toBeInTheDocument();
   });
 
-  it('renders correctly', async () => {
-    const { asFragment } = renderComponent(props);
-    expect(asFragment()).toMatchSnapshot();
+  it('displays as view cards when card view is disabled', () => {
+    render(<ViewButton viewCards={false} handleButtonChange={jest.fn()} />, {
+      wrapper: Wrapper,
+    });
+
+    const button = screen.getByRole('button', {
+      name: 'page view app.view_cards',
+    });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('app.view_cards');
+    expect(screen.getByTestId('ViewAgendaIcon')).toBeInTheDocument();
   });
 
   it('calls the handle button change when the view button is clicked', async () => {
-    renderComponent(props);
+    const user = userEvent.setup();
+    const handleButtonChange = jest.fn();
+
+    render(<ViewButton viewCards handleButtonChange={handleButtonChange} />, {
+      wrapper: Wrapper,
+    });
+
     await user.click(
       await screen.findByRole('button', {
         name: 'page view app.view_table',
@@ -81,16 +95,22 @@ describe('Generic view button', () => {
   });
 
   it('is disabled when prop disabled is equal to true', async () => {
-    props = {
-      viewCards: true,
-      handleButtonChange: handleButtonChange,
-      disabled: true,
-    };
-    renderComponent(props);
-    expect(
-      await screen.findByRole('button', {
-        name: 'page view app.view_table',
-      })
-    ).toBeDisabled();
+    const user = userEvent.setup();
+    const handleButtonChange = jest.fn();
+
+    render(
+      <ViewButton viewCards disabled handleButtonChange={handleButtonChange} />,
+      {
+        wrapper: Wrapper,
+      }
+    );
+
+    const button = screen.getByRole('button', {
+      name: 'page view app.view_table',
+    });
+
+    expect(button).toBeDisabled();
+    await expect(user.click(button)).rejects.toThrowError();
+    expect(handleButtonChange).not.toBeCalled();
   });
 });
