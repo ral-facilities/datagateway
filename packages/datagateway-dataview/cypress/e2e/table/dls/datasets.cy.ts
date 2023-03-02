354465427d2d134be6227127942927cb15b2a748
jounaidr <jounaidruhomaun@googlemail.com>
2023-03-02 13:38:23 +0000

    remove 'and then calculate file size' test cases from e2e
    Signed-off-by: jounaidr <jounaidruhomaun@googlemail.com>

--- packages/datagateway-dataview/cypress/e2e/table/dls/datasets.cy.ts
+++ packages/datagateway-dataview/cypress/e2e/table/dls/datasets.cy.ts
@@ -248,42 +248,6 @@ describe('DLS - Datasets Table', () => {
         .should('be.visible');
     });
 
-    it('and then calculate file size', () => {
-      // need to wait for counts to finish, otherwise cypress might interact with the details panel
-      // too quickly and it rerenders during the test
-      cy.contains('[aria-rowindex="1"] [aria-colindex="4"]', '15').should(
-        'exist'
-      );
-      cy.contains('[aria-rowindex="2"] [aria-colindex="4"]', '15').should(
-        'exist'
-      );
-
-      cy.get('[aria-label="Show details"]').first().click();
-
-      cy.contains('4.55 GB', { timeout: 10000 })
-        .scrollIntoView()
-        .should('be.visible');
-    });
-
-    it('and then calculate file size when the value is 0 ', () => {
-      cy.intercept('**/getSize?*', '0');
-
-      // need to wait for counts to finish, otherwise cypress might interact with the details panel
-      // too quickly and it rerenders during the test
-      cy.contains('[aria-rowindex="1"] [aria-colindex="4"]', '15').should(
-        'exist'
-      );
-      cy.contains('[aria-rowindex="2"] [aria-colindex="4"]', '15').should(
-        'exist'
-      );
-
-      cy.get('[aria-label="Show details"]').first().click();
-
-      cy.contains('0 B', { timeout: 10000 })
-        .scrollIntoView()
-        .should('be.visible');
-    });
-
     it('and view the dataset type panel', () => {
       // need to wait for counts to finish, otherwise cypress might interact with the details panel
       // too quickly and it rerenders during the test
