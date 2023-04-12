114ae63d76d33240ae90db30bd6238995a024428
jounaidr <jounaidruhomaun@googlemail.com>
2023-04-12 13:36:36 +0100

    dataPublications landing e2e
    Signed-off-by: jounaidr <jounaidruhomaun@googlemail.com>

--- packages/datagateway-dataview/cypress/e2e/landing/isis/dataPublication.cy.ts
+++ packages/datagateway-dataview/cypress/e2e/landing/isis/dataPublication.cy.ts
@@ -1,7 +1,7 @@
-describe('ISIS - Study Landing', () => {
+describe('ISIS - Data Publication Landing', () => {
   beforeEach(() => {
     cy.login();
-    cy.visit('/browseStudyHierarchy/instrument/1/study/14');
+    cy.visit('/browseDataPublications/instrument/1/dataPublication/14');
   });
 
   it('should load correctly', () => {
@@ -16,7 +16,7 @@ describe('ISIS - Study Landing', () => {
     cy.get('#study-investigations-tab').first().click({ force: true });
     cy.location('pathname').should(
       'eq',
-      '/browseStudyHierarchy/instrument/1/study/14/investigation'
+      '/browseDataPublications/instrument/1/dataPublication/14/investigation'
     );
   });
 
@@ -27,7 +27,7 @@ describe('ISIS - Study Landing', () => {
       .click({ force: true });
     cy.location('pathname').should(
       'eq',
-      '/browseStudyHierarchy/instrument/1/study/14/investigation/41'
+      '/browseDataPublications/instrument/1/dataPublication/14/investigation/41'
     );
   });
 
@@ -39,72 +39,44 @@ describe('ISIS - Study Landing', () => {
     );
   });
 
-  it('should have the correct urls for the DOI link and Study PID', () => {
+  it('should have the correct urls for the DOI link and Data Publication PID', () => {
     // Study PID
 
-    cy.get('[data-testid="landing-study-pid-link"]')
+    cy.get('[data-testid="landing-dataPublication-pid-link"]')
       .first()
       .then(($pid) => {
         const pid = $pid.text();
 
         const url = `https://doi.org/${pid}`;
 
-        cy.get('[data-testid="landing-study-pid-link"]')
-          .first()
-          .should('have.attr', 'href', url);
-      });
-
-    // Parent DOI
-
-    cy.get('[data-testid="landing-study-doi-link"]')
-      .first()
-      .then(($doi) => {
-        const doi = $doi.text();
-
-        const url = `https://doi.org/${doi}`;
-
-        cy.get('[data-testid="landing-study-doi-link"]')
+        cy.get('[data-testid="landing-dataPublication-pid-link"]')
           .first()
           .should('have.attr', 'href', url);
       });
   });
 
   it('should load correctly when investigation missing', () => {
-    cy.intercept('**/studies?*', [
+    cy.intercept('**/datapublications?*', [
       {
         id: 101224979,
         pid: '10.5286/ISIS.E.RB1810842',
-        studyInvestigations: [
-          {
-            createId: 'uows/1050072',
-            createTime: '2019-02-07 15:27:19.790000+00:00',
-            id: 101224981,
-          },
-        ],
       },
     ]);
-    cy.visit('/browseStudyHierarchy/instrument/1/study/14');
+    cy.visit('/browseDataPublications/instrument/1/dataPublication/14');
     cy.get('#datagateway-dataview').should('be.visible');
     cy.contains('10.5286/ISIS.E.RB1810842').should('be.visible');
   });
 
   it('should disable the hover tool tip by pressing escape', () => {
-    cy.intercept('**/studies?*', [
+    cy.intercept('**/datapublications?*', [
       {
         id: 14,
         pid: '10.5286/ISIS.E.RB1810842',
-        studyInvestigations: [
-          {
-            createId: 'uows/1050072',
-            createTime: '2019-02-07 15:27:19.790000+00:00',
-            id: 10,
-          },
-        ],
       },
     ]);
     // The hover tool tip has a enter delay of 500ms.
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.get('[data-testid="landing-study-pid-link"]')
+    cy.get('[data-testid="landing-dataPublication-pid-link"]')
       .first()
       .trigger('mouseover')
       .wait(700)
@@ -114,7 +86,7 @@ describe('ISIS - Study Landing', () => {
     cy.get('body').type('{esc}');
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.get('[data-testid="landing-study-pid-link"]')
+    cy.get('[data-testid="landing-dataPublication-pid-link"]')
       .wait(700)
       .first()
       .get('[role="tooltip"]')
@@ -122,24 +94,17 @@ describe('ISIS - Study Landing', () => {
   });
 
   it('should be able to use the citation formatter', () => {
-    cy.intercept('**/studies?*', [
+    cy.intercept('**/datapublications?*', [
       {
         id: 101224979,
         pid: '10.5286/ISIS.E.RB1810842',
-        studyInvestigations: [
-          {
-            createId: 'uows/1050072',
-            createTime: '2019-02-07 15:27:19.790000+00:00',
-            id: 101224981,
-          },
-        ],
       },
     ]);
     cy.intercept('**/text/x-bibliography/10.5286/ISIS.E.RB1810842?*', [
       '@misc{dr sabrina gaertner_mr vincent deguin_dr pierre ghesquiere_dr claire...}',
     ]);
 
-    cy.visit('/browseStudyHierarchy/instrument/1/study/14');
+    cy.visit('/browseDataPublications/instrument/1/dataPublication/14');
     cy.get('#datagateway-dataview').should('be.visible');
     cy.contains('10.5286/ISIS.E.RB1810842').should('be.visible');
     cy.get('[data-testid="citation-formatter-citation"]').contains(
@@ -159,23 +124,16 @@ describe('ISIS - Study Landing', () => {
   });
 
   it('citation formatter should give an error when there is a problem', () => {
-    cy.intercept('**/studies?*', [
+    cy.intercept('**/datapublications?*', [
       {
         id: 101224979,
         pid: 'invaliddoi',
-        studyInvestigations: [
-          {
-            createId: 'uows/1050072',
-            createTime: '2019-02-07 15:27:19.790000+00:00',
-            id: 101224981,
-          },
-        ],
       },
     ]);
     cy.intercept('**/text/x-bibliography/invaliddoi?*', {
       statusCode: 503,
     });
-    cy.visit('/browseStudyHierarchy/instrument/1/study/14');
+    cy.visit('/browseDataPublications/instrument/1/dataPublication/14');
     cy.get('#datagateway-dataview').should('be.visible');
     cy.contains('invaliddoi').should('be.visible');
 
