aa1e8038b7ec770302ff349adbbb061cdab3323d
Louise Davies <louise.davies@stfc.ac.uk>
2023-04-19 14:45:49 +0100

    Upgrade or remove non-react 18 compatible packages

--- packages/datagateway-common/src/state/app.types.tsx
+++ packages/datagateway-common/src/state/app.types.tsx
@@ -1,4 +1,3 @@
-import type { RouterState } from 'connected-react-router';
 import type { AnyAction } from 'redux';
 import type { ThunkAction } from 'redux-thunk';
 import type {
@@ -59,7 +58,6 @@ export interface DGCommonState {
 
 export interface StateType {
   dgcommon: DGCommonState;
-  router: RouterState;
 }
 
 export interface ActionType<T> {
