161e8ded9320ebcfc5a9908137383f4371edfa2a
Louise Davies <louise.davies@stfc.ac.uk>
2024-02-23 14:01:29 +0000

    #1152 - remove unused functionality from count endpoints

--- packages/datagateway-common/src/api/investigations.tsx
+++ packages/datagateway-common/src/api/investigations.tsx
@@ -211,16 +211,11 @@ const fetchInvestigationCount = (
 };
 
 export const useInvestigationCount = (
-  additionalFilters?: AdditionalFilters,
-  storedFilters?: FiltersType,
-  currentTab?: string
+  additionalFilters?: AdditionalFilters
 ): UseQueryResult<number, AxiosError> => {
   const apiUrl = useSelector((state: StateType) => state.dgcommon.urls.apiUrl);
   const location = useLocation();
-  const filters =
-    currentTab === 'investigation' || !storedFilters
-      ? parseSearchToQuery(location.search).filters
-      : storedFilters;
+  const filters = parseSearchToQuery(location.search).filters;
 
   return useQuery<
     number,
