fb94bc3ff699a273cf2a60e70764a4f8e42f01e8
akintomiwa <akinonigbinde@gmail.com>
2024-08-20 11:43:59 +0000

    test data update - dataview, download

--- packages/datagateway-dataview/src/views/datafilePreview/testData.ts
+++ packages/datagateway-dataview/src/views/datafilePreview/testData.ts
@@ -3,13 +3,13 @@
 import type { Datafile } from 'datagateway-common';
 
 const mockDatafile = {
-  createTime: '2020-01-01',
+  datafileCreateTime: '2020-01-01',
   dataset: undefined,
   description: 'test description',
   fileSize: 100,
   id: 123,
   location: 'test location',
-  modTime: '2020-01-02',
+  datafileModTime: '2020-01-02',
   name: 'Datafile.txt',
   parameters: [],
 } satisfies Datafile;
