bd9ff26550fe02fd30b322bfb901caa443607051
Louise Davies <louise.davies@stfc.ac.uk>
2025-06-12 11:04:13 +0100

    Fix useSticky test - actually mock lodash.debounce - fix missing await/async - move shared setup to beforeEach

--- packages/datagateway-common/src/hooks/useSticky.test.ts
+++ packages/datagateway-common/src/hooks/useSticky.test.ts
@@ -1,69 +1,72 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { fireEvent, renderHook, waitFor } from '@testing-library/react';
+import { Mock } from 'vitest';
 import useSticky from './useSticky';
-import { fireEvent } from '@testing-library/react';
+
+// Mock lodash.debounce to return the function we want to call without delay
+vi.mock('lodash.debounce', () => ({
+  default: (fn: (args: unknown) => unknown) => fn,
+}));
 
 describe('useSticky', () => {
-  it('returns isSticky true if the bottom of the target element is scrolled past', () => {
-    const mockGetBoundingClientRect = vi.fn();
-    const target = document.createElement('div');
+  let mockGetBoundingClientRect: Mock<() => DOMRect>;
+  let target: HTMLDivElement;
+  const initScrollY = global.scrollY;
+
+  beforeEach(() => {
+    mockGetBoundingClientRect = vi.fn(() => new DOMRect(0, 0, 0, 100));
+    target = document.createElement('div');
     target.getBoundingClientRect = mockGetBoundingClientRect;
 
+    global.scrollY = 100;
+  });
+
+  afterEach(() => {
+    global.scrollY = initScrollY;
+  });
+
+  it('returns isSticky true if the bottom of the target element is scrolled past', async () => {
     // if window scrollY is greater than boundingClientRect bottom
     // that means the element is scrolled out of view
-    global.scrollY = 100;
-    mockGetBoundingClientRect.mockReturnValue({
-      bottom: 50,
-    });
+    mockGetBoundingClientRect.mockReturnValue(new DOMRect(0, 0, 0, 50));
 
     const { result } = renderHook(() => useSticky({ current: target }));
 
     // scroll the window
     fireEvent.scroll(window, {});
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(result.current.isSticky).toBe(true);
     });
   });
 
-  it('returns isSticky false if the bottom of the target element is still in view', () => {
-    const mockGetBoundingClientRect = vi.fn();
-    const target = document.createElement('div');
-    target.getBoundingClientRect = mockGetBoundingClientRect;
-
+  it('returns isSticky false if the bottom of the target element is still in view', async () => {
     // if window scrollY is greater than boundingClientRect bottom
     // that means the element is scrolled out of view
-    global.scrollY = 100;
-    mockGetBoundingClientRect.mockReturnValue({
-      bottom: 150,
-    });
+    mockGetBoundingClientRect.mockReturnValue(new DOMRect(0, 0, 0, 150));
 
     const { result } = renderHook(() => useSticky({ current: target }));
 
     // scroll the window
     fireEvent.scroll(window, {});
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(result.current.isSticky).toBe(false);
     });
   });
 
-  it('returns isSticky false upon window scroll if the given react ref points to null', () => {
+  it('returns isSticky false upon window scroll if the given react ref points to null', async () => {
     const { result } = renderHook(() => useSticky({ current: null }));
     // scroll the window
     fireEvent.scroll(window, {});
-    waitFor(() => {
+    await waitFor(() => {
       expect(result.current.isSticky).toBe(false);
     });
   });
 
-  it('returns isStick false upon window scroll if bottom coordinate of the target element is unavailable', () => {
-    const mockGetBoundingClientRect = vi.fn();
-    const target = document.createElement('div');
-    target.getBoundingClientRect = mockGetBoundingClientRect;
-
+  it('returns isStick false upon window scroll if bottom coordinate of the target element is unavailable', async () => {
     // if window scrollY is greater than boundingClientRect bottom
     // that means the element is scrolled out of view
-    global.scrollY = 100;
+    // @ts-expect-error purposefully providing an invalid value
     mockGetBoundingClientRect.mockReturnValue({
       top: 150,
     });
@@ -73,7 +76,7 @@ describe('useSticky', () => {
     // scroll the window
     fireEvent.scroll(window, {});
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(result.current.isSticky).toBe(false);
     });
   });
