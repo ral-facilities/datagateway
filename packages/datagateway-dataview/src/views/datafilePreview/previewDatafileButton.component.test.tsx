451424fde6a8c0aec79e1fcce80bb78f89db8599
Louise Davies <louise.davies@stfc.ac.uk>
2025-10-06 15:05:54 +0100

    Disable datafile previews if disableAnonDownload enabled

--- packages/datagateway-dataview/src/views/datafilePreview/previewDatafileButton.component.test.tsx
+++ packages/datagateway-dataview/src/views/datafilePreview/previewDatafileButton.component.test.tsx
@@ -1,35 +1,66 @@
-import type { Datafile } from 'datagateway-common';
-import { render, screen } from '@testing-library/react';
+import { RenderResult, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import {
+  dGCommonInitialState,
+  readSciGatewayToken,
+  type Datafile,
+  type StateType,
+} from 'datagateway-common';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import PreviewDatafileButton from './previewDatafileButton.component';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import PreviewDatafileButton, {
+  PreviewDatafileButtonProps,
+} from './previewDatafileButton.component';
 import { mockDatafile } from './testData';
 
+vi.mock('datagateway-common', async () => ({
+  ...(await vi.importActual('datagateway-common')),
+  readSciGatewayToken: vi
+    .fn()
+    .mockReturnValue({ sessionId: 'abcdef', username: 'test' }),
+}));
+
 describe('PreviewDatafileButton', () => {
+  const mockStore = configureStore([thunk]);
+  let state: StateType;
   let user: ReturnType<typeof userEvent.setup>;
+
+  function renderComponent(props: PreviewDatafileButtonProps): RenderResult {
+    const store = mockStore(state);
+    return render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PreviewDatafileButton {...props} />
+        </MemoryRouter>
+      </Provider>
+    );
+  }
 
   beforeEach(() => {
     user = userEvent.setup();
+
+    state = JSON.parse(
+      JSON.stringify({
+        dgdataview: {}, //Dont need to fill, since not part of the test
+        dgcommon: {
+          ...dGCommonInitialState,
+        },
+      })
+    );
   });
 
   describe('given a supported datafile', () => {
     it('should be enabled', () => {
-      render(
-        <MemoryRouter>
-          <PreviewDatafileButton datafile={mockDatafile} />
-        </MemoryRouter>
-      );
+      renderComponent({ datafile: mockDatafile });
       expect(
         screen.getByRole('link', { name: 'datafiles.preview.preview_datafile' })
       ).toBeEnabled();
     });
 
     it('should show a tooltip indicating that this button opens the preview of the datafile when hovered over', async () => {
-      render(
-        <MemoryRouter>
-          <PreviewDatafileButton datafile={mockDatafile} />
-        </MemoryRouter>
-      );
+      renderComponent({ datafile: mockDatafile });
 
       await user.hover(
         screen.getByRole('link', { name: 'datafiles.preview.preview_datafile' })
@@ -48,11 +79,8 @@ describe('PreviewDatafileButton', () => {
     };
 
     it('should be disabled', async () => {
-      render(
-        <MemoryRouter>
-          <PreviewDatafileButton datafile={unsupportedDatafile} />
-        </MemoryRouter>
-      );
+      renderComponent({ datafile: unsupportedDatafile });
+
       // toBeDisabled does not support aria-disabled
       // https://github.com/testing-library/jest-dom/issues/144
       expect(
@@ -63,11 +91,7 @@ describe('PreviewDatafileButton', () => {
     });
 
     it('should show a tooltip indicating that this datafile cannot be previewed when hovered over', async () => {
-      render(
-        <MemoryRouter>
-          <PreviewDatafileButton datafile={unsupportedDatafile} />
-        </MemoryRouter>
-      );
+      renderComponent({ datafile: unsupportedDatafile });
 
       // link cannot be hovered directly because it is disabled
       // we find its container div by its label (which is the same as the tooltip text)
@@ -79,5 +103,31 @@ describe('PreviewDatafileButton', () => {
         await screen.findByText('datafiles.preview.preview_unsupported')
       ).toBeInTheDocument();
     });
+  });
+
+  it('renders as disabled when logged in as anon and disableAnonDownload is true', async () => {
+    state.dgcommon.features = { disableAnonDownload: true };
+    vi.mocked(readSciGatewayToken).mockReturnValue({
+      sessionId: 'abcdef',
+      username: 'anon/anon',
+    });
+
+    renderComponent({ datafile: mockDatafile });
+
+    const previewButton = screen.getByRole('link', {
+      name: 'datafiles.preview.preview_datafile',
+    });
+
+    expect(
+      screen.getByRole('link', {
+        name: 'datafiles.preview.preview_datafile',
+      })
+    ).toHaveAttribute('aria-disabled', 'true');
+
+    await user.hover(previewButton.parentElement);
+
+    expect(
+      await screen.findByText('buttons.disallow_anon_tooltip')
+    ).toBeInTheDocument();
   });
 });
