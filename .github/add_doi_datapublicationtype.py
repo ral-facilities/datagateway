8d83259bfd0ed90a38fdb7caa1ed964d60b3c888
Louise Davies <louise.davies@stfc.ac.uk>
2025-12-16 15:41:07 +0000

    Add fix to remove funderIdentifier from CI data as it breaks the API

--- .github/add_doi_datapublicationtype.py
+++ .github/add_doi_datapublicationtype.py
@@ -17,3 +17,8 @@ data_publication_type_2.name = "Investigation"
 data_publication_type_2.description = "Investigation"
 data_publication_type_2.facility = client.get("Facility", 1)
 data_publication_type_2.create()
+
+# TODO: remove when this is fixed by API
+for funding_reference in client.search("SELECT fr FROM FundingReference fr JOIN fr.investigations AS fi JOIN fi.investigation AS i WHERE i.id = 15"):
+    funding_reference.funderIdentifier = None
+    funding_reference.update()
